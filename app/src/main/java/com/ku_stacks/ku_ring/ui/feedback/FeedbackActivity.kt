51fc3530315a5a9e60fbe322bd52cca2d3be9271
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-10-15 21:56:51 +0900

    [디자인 수정] FeedbackActivity에서 키보드 올라왔을 때 상단 이미지 안보이게 수정

--- app/src/main/java/com/ku_stacks/ku_ring/ui/feedback/FeedbackActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/feedback/FeedbackActivity.kt
@@ -1,12 +1,21 @@
 package com.ku_stacks.ku_ring.ui.feedback
 
+import android.app.Service
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.databinding.ActivityFeedbackBinding
+import com.ku_stacks.ku_ring.util.SlideAnimation
+import com.ku_stacks.ku_ring.util.SoftKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class FeedbackActivity : AppCompatActivity() {
@@ -14,10 +23,13 @@ class FeedbackActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedbackBinding
     private val viewModel by viewModels<FeedbackViewModel>()
 
+    private var softKeyboard: SoftKeyboard? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupBinding()
+        setupKeyboardListener()
         observeData()
     }
 
@@ -31,5 +43,35 @@ class FeedbackActivity : AppCompatActivity() {
         viewModel.quit.observe(this) {
             finish()
         }
+    }
+
+    private fun setupKeyboardListener() {
+        val controlManager = getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
+        val softKeyboard = SoftKeyboard(binding.feedbackLayout, controlManager)
+        softKeyboard.setSoftKeyboardCallback(object: SoftKeyboard.SoftKeyboardChanged {
+            override fun onSoftKeyboardHide() {
+                Timber.e("on keyboard hide")
+
+                lifecycleScope.launch {
+                    SlideAnimation.expand(binding.feedbackChatImg)
+                    SlideAnimation.expand(binding.feedbackTitleTxt)
+                }
+            }
+
+            override fun onSoftKeyboardShow() {
+                Timber.e("on keyboard show")
+
+                lifecycleScope.launch {
+                    SlideAnimation.collapse(binding.feedbackChatImg)
+                    SlideAnimation.collapse(binding.feedbackTitleTxt)
+                }
+            }
+
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        softKeyboard?.unRegisterSoftKeyboardCallback()
     }
 }
