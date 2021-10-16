268770c3d4623cf652c0e50a47fe21692d59eaed
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-10-16 18:22:50 +0900

    [버그] FeedbackActivity에서 키보드올라온 채로 화면 회전 등의 상황에서 crash나는 이슈 해결

--- app/src/main/java/com/ku_stacks/ku_ring/ui/feedback/FeedbackActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/feedback/FeedbackActivity.kt
@@ -1,27 +1,20 @@
 package com.ku_stacks.ku_ring.ui.feedback
 
-import android.app.Service
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
 import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.databinding.ActivityFeedbackBinding
-import com.ku_stacks.ku_ring.util.AppearanceAnimation
-import com.ku_stacks.ku_ring.util.SoftKeyboard
+import com.ku_stacks.ku_ring.util.AppearanceAnimator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import timber.log.Timber
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 
 @AndroidEntryPoint
 class FeedbackActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFeedbackBinding
     private val viewModel by viewModels<FeedbackViewModel>()
-
-    private var softKeyboard: SoftKeyboard? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,32 +37,20 @@ class FeedbackActivity : AppCompatActivity() {
     }
 
     private fun setupKeyboardListener() {
-        val controlManager = getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
-        val softKeyboard = SoftKeyboard(binding.feedbackLayout, controlManager)
-        softKeyboard.setSoftKeyboardCallback(object: SoftKeyboard.SoftKeyboardChanged {
-            override fun onSoftKeyboardHide() {
-                Timber.e("on keyboard hide")
-
-                lifecycleScope.launch {
-                    AppearanceAnimation.expand(binding.feedbackChatImg)
-                    AppearanceAnimation.expand(binding.feedbackTitleTxt)
-                }
+        KeyboardVisibilityEvent.setEventListener(this, this) { isOpen ->
+            if (isOpen) {
+                AppearanceAnimator.collapse(binding.feedbackChatImg)
+                AppearanceAnimator.collapse(binding.feedbackTitleTxt)
+            } else {
+                val initialHeightOfFeedbackImg = 110
+                AppearanceAnimator.expand(binding.feedbackChatImg, initialHeightOfFeedbackImg)
+                AppearanceAnimator.expand(binding.feedbackTitleTxt)
             }
+        }
 
-            override fun onSoftKeyboardShow() {
-                Timber.e("on keyboard show")
-
-                lifecycleScope.launch {
-                    AppearanceAnimation.collapse(binding.feedbackChatImg)
-                    AppearanceAnimation.collapse(binding.feedbackTitleTxt)
-                }
-            }
-
-        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        softKeyboard?.unRegisterSoftKeyboardCallback()
     }
 }
