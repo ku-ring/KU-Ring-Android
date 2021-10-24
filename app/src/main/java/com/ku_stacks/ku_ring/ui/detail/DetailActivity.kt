66a94694cd2199ae9dec609ed0c21b5aee2f3abe
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-10-24 23:42:14 +0900

    [추가] DetailActivity 와 WebView모듈 추가

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/detail/DetailActivity.kt
@@ -0,0 +1,42 @@
+package com.ku_stacks.ku_ring.ui.detail
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import com.ku_stacks.ku_ring.R
+import android.webkit.WebSettings
+
+import android.webkit.WebViewClient
+
+
+
+
+class DetailActivity: AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        webView = findViewById(R.id.detail_webView)
+        webView.webViewClient = WebViewClient() // 클릭시 새창 안뜨게
+
+        webView.settings.apply {
+            javaScriptEnabled = true // 웹페이지 자바스클비트 허용 여부
+            setSupportMultipleWindows(false) // 새창 띄우기 허용 여부
+            javaScriptCanOpenWindowsAutomatically = false // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+            loadWithOverviewMode = true // 메타태그 허용 여부
+            useWideViewPort = true // 화면 사이즈 맞추기 허용 여부
+            setSupportZoom(false) // 화면 줌 허용 여부
+            builtInZoomControls = false // 화면 확대 축소 허용 여부
+            layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN // 컨텐츠 사이즈 맞추기
+            cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK // 브라우저 캐시 허용 여부
+            domStorageEnabled = true // 로컬저장소 허용 여부
+        }
+
+        webView.loadUrl("https://naver.com") // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+    }
+}
