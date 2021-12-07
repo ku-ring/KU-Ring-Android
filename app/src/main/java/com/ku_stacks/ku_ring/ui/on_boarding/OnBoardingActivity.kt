81ca91336256fe2e39eb7b4ba4888497db2b01e9
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-12-07 17:37:04 +0900

    [추가] 온보딩 페이지 화면 구축

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/on_boarding/OnBoardingActivity.kt
@@ -0,0 +1,10 @@
+package com.ku_stacks.ku_ring.ui.on_boarding
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnBoardingActivity : AppCompatActivity() {
+
+
+}
