c956ae52f53ef814c9d03c3a9305260bf4050879
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-12-15 20:40:16 +0900

    [테스트] convertDateToDay 테스트 코드 추가

--- app/src/test/java/com/ku_stacks/ku_ring/util_usecase/DateUtilTest.kt
+++ app/src/test/java/com/ku_stacks/ku_ring/util_usecase/DateUtilTest.kt
@@ -9,4 +9,11 @@ class DateUtilTest {
     fun getTodayTest() {
         print(DateUtil.getToday())
     }
+
+    @Test
+    fun convertDateToDayTest() {
+        val date = "2021-12-14 17:11:18"
+        print(DateUtil.convertDateToDay(date))
+        //형식 잘못되면 파싱 오류 -> 2020.11.02 (월) 이렇게 뜸
+    }
 }
