8aafecee9b1be84ce7414f6e6d061ca929f2fb1c
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-01-04 22:12:12 +0900

    [수정] SplashActivity에서 BuildConfig로 접근하여 버전 보여주도록 수정

--- app/src/main/java/com/ku_stacks/ku_ring/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/splash/SplashActivity.kt
@@ -3,8 +3,10 @@ package com.ku_stacks.ku_ring.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.ku_stacks.ku_ring.R
+import com.ku_stacks.ku_ring.databinding.ActivitySplashBinding
 import com.ku_stacks.ku_ring.ui.home.HomeActivity
 import com.ku_stacks.ku_ring.ui.on_boarding.OnBoardingActivity
 import com.ku_stacks.ku_ring.util.PreferenceUtil
@@ -19,15 +21,18 @@ class SplashActivity : AppCompatActivity() {
     @Inject
     lateinit var pref: PreferenceUtil
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        binding.lifecycleOwner = this
 
         lifecycleScope.launch {
             delay(1000)
 
-            //저장한 구독목록이 있거나 firstRunFlag 가 false 면 바로 홈화면
+            // 저장한 구독목록이 있거나 firstRunFlag 가 false 면 바로 홈화면 진입
             if (!pref.firstRunFlag || !pref.subscription.isNullOrEmpty()) {
                 startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
                 overridePendingTransition(0, 0)
