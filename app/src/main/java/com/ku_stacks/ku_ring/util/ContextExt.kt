dc855f0bad274c4a3670b7842932eee37130c160
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-01-13 19:23:53 +0900

    [수정] 웹뷰에서 디바이스의 Android 버전(Q 기준)에 따른 외부저장소 권한 요청 처리

--- app/src/main/java/com/ku_stacks/ku_ring/util/ContextExt.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/util/ContextExt.kt
@@ -5,3 +5,6 @@ import android.widget.Toast
 
 fun Context.showToast(msg: String) =
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+fun Context.showToastLong(msg: String) =
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
