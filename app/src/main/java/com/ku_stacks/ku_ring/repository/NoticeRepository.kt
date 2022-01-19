ac740d9d1500214979423e8c83369601cccfe8c5
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-01-19 22:30:00 +0900

    [수정] NoticeRepository에서 copy()를 이용한 코드 정리

--- app/src/main/java/com/ku_stacks/ku_ring/repository/NoticeRepository.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/repository/NoticeRepository.kt
@@ -30,12 +30,16 @@ class NoticeRepository @Inject constructor(
             .map { transformRemoteData(it, type) }
             .cachedIn(scope)
 
-        // 하나의 insert에 대해서 2개 또는 3개의 변화 감지가 발생할 것임.
-        // 그 이유는 양옆 fragment의 viewModel에서 호출하기 때문
+        /**
+         하나의 insert에 대해서 2개 또는 3개의 변화 감지가 발생할 것임.
+         그 이유는 양옆 fragment 의 viewModel 에서 호출하기 때문
+        */
         val flowableLocal = noticeDao.getReadNoticeRecord(true)
             .distinctUntilChanged { old, new ->
-                // DB insert 되는 경우, 업데이트를 감지하기 위함이므로 성능을 위해
-                // 모든 내용을 비교하기 보다는 size 만 비교하는 것으로 재정의함.
+                /**
+                 DB insert 되는 경우, 업데이트를 감지하기 위함이므로 성능을 위해
+                 모든 내용을 비교하기 보다는 size 만 비교하는 것으로 재정의함.
+                */
                 old.size == new.size
             }
 
@@ -43,63 +47,39 @@ class NoticeRepository @Inject constructor(
             flowableRemote,
             flowableLocal,
             { remote, local -> //isRead 의 변동이 있을때 알맞게 변형시켜야함
-                remote.map {
-                    if (local.contains(it.articleId)) {
-                        return@map Notice(
-                            postedDate = it.postedDate,
-                            subject = it.subject,
-                            category = it.category,
-                            url = it.url,
-                            articleId = it.articleId,
-                            isNew = it.isNew,
-                            isRead = true,
-                            isSubscribing = it.isSubscribing,
-                            tag = it.tag
-                        )
-                    } else {
-                        return@map it
-                    }
+                remote.map { notice ->
+                    notice.copy(isRead = local.contains(notice.articleId))
                 }
             })
     }
 
-    private fun transformRemoteData(pagingData: PagingData<Notice>, type: String): PagingData<Notice> {
+    private fun transformRemoteData(
+        pagingData: PagingData<Notice>,
+        type: String,
+    ): PagingData<Notice> {
         val startDate = pref.startDate
         val subscribingSet = pref.subscription
         val isSubscribing = subscribingSet?.contains(type) == true
 
-        if (startDate.isNullOrEmpty() || DateUtil.isToday(startDate)) { // 설치 이후 앱을 처음 킨 경우
+        if (startDate.isNullOrEmpty() || DateUtil.isToday(startDate)) {
+            /** 설치 이후 앱을 처음 킨 경우 */
             Timber.e("This is first connect day")
             if (startDate.isNullOrEmpty()) {
                 pref.startDate = DateUtil.getToday()
             }
-            return pagingData.map {
-                val isRead = noticeDao.isReadNotice(it.articleId)
-                Notice(
-                    postedDate = it.postedDate,
-                    subject = it.subject,
-                    category = it.category,
-                    url = it.url,
-                    articleId = it.articleId,
-                    isNew = DateUtil.isToday(it.postedDate) && !isRead,
-                    isRead = isRead,
-                    isSubscribing = isSubscribing,
-                    tag = it.tag
+            return pagingData.map { notice ->
+                notice.copy(
+                    isNew = DateUtil.isToday(notice.postedDate) && !noticeDao.isReadNotice(notice.articleId),
+                    isSubscribing = isSubscribing
                 )
             }
-        } else { //앱을 처음 킨 것이 아닌 경우(일반적인 케이스)
+        } else {
+            /** 앱을 처음 킨 것이 아닌 경우(일반적인 케이스) */
             Timber.e("This is not first connect day")
-            return pagingData.map {
-                Notice(
-                    postedDate = it.postedDate,
-                    subject = it.subject,
-                    category = it.category,
-                    url = it.url,
-                    articleId = it.articleId,
-                    isNew = it.postedDate > startDate && !isNewRecordHashMap.containsKey(it.articleId),
-                    isRead = noticeDao.isReadNotice(it.articleId),
-                    isSubscribing = isSubscribing,
-                    tag = it.tag
+            return pagingData.map { notice ->
+                notice.copy(
+                    isNew = notice.postedDate > startDate && !isNewRecordHashMap.containsKey(notice.articleId),
+                    isSubscribing = isSubscribing
                 )
             }
         }
@@ -121,7 +101,7 @@ class NoticeRepository @Inject constructor(
     private fun getFlowableRemoteNotice(type: String): Flowable<PagingData<Notice>>{
          return Pager(
             config = PagingConfig(
-                pageSize = 20,  //이것보다 PagingSource 에서 ItemCount 가 중요함
+                pageSize = 20,  /** 이것보다 PagingSource 에서 ItemCount 가 중요함 */
                 enablePlaceholders = true
             ),
             pagingSourceFactory = { NoticePagingSource(type, noticeClient.noticeService) }
