0405341fee3a54b993b30acbb9b8945e395acdc2
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-01-20 17:09:56 +0900

    [수정] BindingAdapter의 Object 상위클래스 없애고 함수로만 구성

--- app/src/main/java/com/ku_stacks/ku_ring/adapter/BindingAdapters.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/adapter/BindingAdapters.kt
@@ -10,103 +10,92 @@ import androidx.databinding.BindingAdapter
 import com.ku_stacks.ku_ring.R
 import timber.log.Timber
 
-object BindingAdapters {
-    @JvmStatic
-    @BindingAdapter("timeMillis")
-    fun TextView.getDay(str: String) {
-        text = when (str.length) {
-            8 -> { // "20211018"
-                str.substring(0, 4) + "." + str.substring(4, 6) + "." + str.substring(6, 8)
-            }
-            19 -> { // "2021-10-10 21:37:37"
-                str.substring(0, 4) + "." + str.substring(5, 7) + "." + str.substring(8, 10)
-            }
-            else -> {
-                Timber.e("timeMillis String length is not normal in BindingAdapter")
-                str
-            }
+@BindingAdapter("timeMillis")
+fun TextView.getDay(str: String) {
+    text = when (str.length) {
+        8 -> { // "20211018"
+            str.substring(0, 4) + "." + str.substring(4, 6) + "." + str.substring(6, 8)
+        }
+        19 -> { // "2021-10-10 21:37:37"
+            str.substring(0, 4) + "." + str.substring(5, 7) + "." + str.substring(8, 10)
+        }
+        else -> {
+            Timber.e("timeMillis String length is not normal in BindingAdapter")
+            str
         }
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("visibleIf")
-    fun View.visibleIf(value: Boolean) {
-        visibility = when (value) {
-            true -> View.VISIBLE
-            else -> View.GONE
+@BindingAdapter("visibleIf")
+fun View.visibleIf(value: Boolean) {
+    visibility = when (value) {
+        true -> View.VISIBLE
+        else -> View.GONE
+    }
+}
+
+@BindingAdapter(value = ["isNew", "isRead", "isSubscribing"])
+fun View.pointColor(isNew: Boolean, isRead: Boolean, isSubscribing: Boolean) {
+    visibility = when {
+        isRead -> {
+            View.GONE
+        }
+        isNew -> {
+            View.VISIBLE
+        }
+        else -> {
+            View.GONE
         }
     }
-
-    @JvmStatic
-    @BindingAdapter(value = ["isNew", "isRead", "isSubscribing"])
-    fun View.pointColor(isNew: Boolean, isRead: Boolean, isSubscribing: Boolean) {
-        visibility = when {
-            isRead -> {
-                View.GONE
-            }
-            isNew -> {
-                View.VISIBLE
-            }
-            else -> {
-                View.GONE
-            }
-        }
-        background = when (isSubscribing) {
-            true -> ContextCompat.getDrawable(this.context, R.drawable.point_primary_pink)
-            else -> ContextCompat.getDrawable(this.context, R.drawable.point_primary_gray)
-        }
+    background = when (isSubscribing) {
+        true -> ContextCompat.getDrawable(this.context, R.drawable.point_primary_pink)
+        else -> ContextCompat.getDrawable(this.context, R.drawable.point_primary_gray)
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("backgroundGrayIf")
-    fun View.backgroundGrayIf(value: Boolean) {
-        if(value) {
-            setBackgroundColor(Color.parseColor("#14000000"))
-        }
-        else {
-            setBackgroundColor(Color.parseColor("#ffffff"))
-        }
+@BindingAdapter("backgroundGrayIf")
+fun View.backgroundGrayIf(value: Boolean) {
+    if (value) {
+        setBackgroundColor(Color.parseColor("#14000000"))
+    } else {
+        setBackgroundColor(Color.parseColor("#ffffff"))
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("textColorGrayIf")
-    fun TextView.textColorGrayIf(value: Boolean) {
-        if (value) {
-            setTextColor(Color.parseColor("#ababab"))
-        } else {
-            setTextColor(Color.parseColor("#000000"))
-        }
+@BindingAdapter("textColorGrayIf")
+fun TextView.textColorGrayIf(value: Boolean) {
+    if (value) {
+        setTextColor(Color.parseColor("#ababab"))
+    } else {
+        setTextColor(Color.parseColor("#000000"))
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("confirmedImageIf")
-    fun ImageView.confirmedImageIf(value: Boolean) {
-        if (value) {
-            setImageResource(R.drawable.ic_check)
-        } else {
-            setImageResource(R.drawable.ic_close)
-        }
+@BindingAdapter("confirmedImageIf")
+fun ImageView.confirmedImageIf(value: Boolean) {
+    if (value) {
+        setImageResource(R.drawable.ic_check)
+    } else {
+        setImageResource(R.drawable.ic_close)
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("enableFeedbackButtonIf")
-    fun Button.enableFeedbackButtonIf(value: Boolean) {
-        background = if (value) {
-            ContextCompat.getDrawable(this.context, R.drawable.button_primary_gradation)
-        } else {
-            ContextCompat.getDrawable(this.context, R.drawable.button_primary_gradation_disabled)
-        }
+@BindingAdapter("enableFeedbackButtonIf")
+fun Button.enableFeedbackButtonIf(value: Boolean) {
+    background = if (value) {
+        ContextCompat.getDrawable(this.context, R.drawable.button_primary_gradation)
+    } else {
+        ContextCompat.getDrawable(this.context, R.drawable.button_primary_gradation_disabled)
     }
+}
 
-    @JvmStatic
-    @BindingAdapter("disableStartButtonIf")
-    fun Button.disableStartButtonIf(value: Boolean) {
-        if (value) {
-            background = ContextCompat.getDrawable(this.context, R.drawable.button_white_disabled)
-            setTextColor(Color.parseColor("#3dbd80"))
-        } else {
-            background = ContextCompat.getDrawable(this.context, R.drawable.button_white)
-            setTextColor(Color.parseColor("#036b3f"))
-        }
+@BindingAdapter("disableStartButtonIf")
+fun Button.disableStartButtonIf(value: Boolean) {
+    if (value) {
+        background = ContextCompat.getDrawable(this.context, R.drawable.button_white_disabled)
+        setTextColor(Color.parseColor("#3dbd80"))
+    } else {
+        background = ContextCompat.getDrawable(this.context, R.drawable.button_white)
+        setTextColor(Color.parseColor("#036b3f"))
     }
 }
