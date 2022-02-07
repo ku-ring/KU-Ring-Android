34d441eeaf7640e9230926536da8201159dbe3de
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-02-07 17:26:52 +0900

    [테스트] SubscribeRepository Test 추가

--- app/src/test/java/com/ku_stacks/ku_ring/MockUtil.kt
+++ app/src/test/java/com/ku_stacks/ku_ring/MockUtil.kt
@@ -1,7 +1,10 @@
 package com.ku_stacks.ku_ring
 
+import com.ku_stacks.ku_ring.data.api.request.SubscribeRequest
+import com.ku_stacks.ku_ring.data.api.response.DefaultResponse
 import com.ku_stacks.ku_ring.data.api.response.NoticeListResponse
 import com.ku_stacks.ku_ring.data.api.response.NoticeResponse
+import com.ku_stacks.ku_ring.data.api.response.SubscribeListResponse
 import com.ku_stacks.ku_ring.data.db.NoticeEntity
 import com.ku_stacks.ku_ring.data.db.PushEntity
 
@@ -39,10 +42,30 @@ object MockUtil {
     fun mockPushEntity() = PushEntity(
         articleId = "5b4a11b",
         category = "bachelor",
-        postedDate = "!",
-        subject = "!",
-        baseUrl = "!",
+        postedDate = "20220203",
+        subject = "2022학년도 1학기 재입학 합격자 유의사항 안내",
+        baseUrl = "https://www.konkuk.ac.kr/do/MessageBoard/ArticleRead.do",
         isNew = false,
-        receivedDate = "!"
+        receivedDate = "20220207-132051"
     )
+
+    fun mockSubscribeListResponse() = SubscribeListResponse(
+        isSuccess = true,
+        resultMsg = "성공",
+        resultCode = 200,
+        categoryList = listOf("bachelor", "employment")
+   )
+
+    fun mockSubscribeRequest() = SubscribeRequest(
+        token = "AAAAn6eQM_Y:APA91bES4rjrFwPY5i_Hz-kT0u32SzIUxreYm9qaQHZeYKGGV_BmHZNJhHvlDjyQA6LveNdxCVrwzsq78jgsnCw8OumbtM5L3cc17XgdqZ_dlpsPzR7TlJwBFTXRFLPst663IeX27sb0",
+        categories = listOf("bachelor", "scholarship")
+    )
+
+    fun mockDefaultResponse() = DefaultResponse(
+        isSuccess = true,
+        resultMsg = "성공",
+        resultCode = 200
+    )
+
+
 }
---
+++ app/src/test/java/com/ku_stacks/ku_ring/repository/SubscribeRepositoryTest.kt
@@ -0,0 +1,85 @@
+package com.ku_stacks.ku_ring.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import com.ku_stacks.ku_ring.MockUtil.mockDefaultResponse
+import com.ku_stacks.ku_ring.MockUtil.mockSubscribeListResponse
+import com.ku_stacks.ku_ring.MockUtil.mockSubscribeRequest
+import com.ku_stacks.ku_ring.data.api.NoticeClient
+import com.ku_stacks.ku_ring.util.PreferenceUtil
+import com.ku_stacks.ku_ring.util.WordConverter
+import io.reactivex.rxjava3.core.Single
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [23])
+class SubscribeRepositoryTest {
+
+    private lateinit var repository: SubscribeRepository
+    private val client: NoticeClient = Mockito.mock(NoticeClient::class.java)
+    private lateinit var pref: PreferenceUtil
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        pref = PreferenceUtil(getApplicationContext())
+        repository = SubscribeRepositoryImpl(client, pref)
+    }
+
+    @Test
+    fun `fetch Subscription From Remote Test`() {
+        // given
+        val mockToken = "AAAAn6eQM_Y:APA91bES4rjrFwPY5i_Hz-kT0u32SzIUxreYm9qaQHZeYKGGV_BmHZNJhHvlDjyQA6LveNdxCVrwzsq78jgsnCw8OumbtM5L3cc17XgdqZ_dlpsPzR7TlJwBFTXRFLPst663IeX27sb0"
+        val mockSubscribeList = mockSubscribeListResponse()
+
+        Mockito.`when`(client.fetchSubscribe(mockToken)).thenReturn(Single.just(mockSubscribeList))
+        val expected = mockSubscribeList.categoryList.map { category ->
+            WordConverter.convertEnglishToKorean(category)
+        }
+
+        // when + then
+        repository.fetchSubscriptionFromRemote(mockToken)
+            .test()
+            .assertNoErrors()
+            .assertValue(expected)
+
+        Mockito.verify(client, Mockito.atLeastOnce()).fetchSubscribe(mockToken)
+    }
+
+    @Test
+    fun `save Subscription To Remote Test`() {
+        // given
+        val mockRequest = mockSubscribeRequest()
+        val mockResponse = mockDefaultResponse()
+
+        Mockito.`when`(client.saveSubscribe(mockRequest)).thenReturn(Single.just(mockResponse))
+
+        // when + then
+        repository.saveSubscriptionToRemote(mockRequest)
+            .test()
+            .assertNoErrors()
+            .assertValue(mockResponse)
+    }
+
+    @Test
+    fun `save Subscription To Local Test`() {
+        // given
+        val mockData = arrayListOf("학사", "취창업")
+
+        // when
+        repository.saveSubscriptionToLocal(mockData)
+
+        // then
+        val expected = arrayListOf("bch", "emp").toSet()
+        assertEquals(expected, pref.subscription)
+    }
+}
