3997d4757620156bb352a96a259433dc6006627b
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-02-11 03:39:33 +0900

    [테스트] TestingLiveDataExt 추가, NotificationViewModelTest에서 getMyNotification() 테스트 추가

--- app/src/test/java/com/ku_stacks/ku_ring/viewmodel_usecase/ViewModelTest.kt
+++ app/src/test/java/com/ku_stacks/ku_ring/viewmodel_usecase/ViewModelTest.kt
@@ -7,7 +7,7 @@ class ViewModelTest {
 
     // 참고할 자료 https://github.com/yeon-kyu/architecture-components-samples/blob/main/BasicRxJavaSampleKotlin/app/src/test/java/com/example/android/observability/UserViewModelTest.kt
 // https://github.com/android/architecture-components-samples/blob/main/PagingSample/app/src/androidTest/java/paging/android/example/com/pagingsample/CheeseViewModelTest.kt
-
+// https://medium.com/androiddevelopers/unit-testing-livedata-and-other-common-observability-problems-bb477262eb04
 
 
 }
