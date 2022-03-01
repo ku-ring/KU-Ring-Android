18476d8335760bab6daa304bcfd0617d1947e4d3
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-03-02 01:27:11 +0900

    [이동] Push -> PushUiModel 로 변환하는 함수들 mapper 폴더로 이동

---
+++ app/src/main/java/com/ku_stacks/ku_ring/data/mapper/ModelToUiModelMapper.kt
@@ -0,0 +1,38 @@
+package com.ku_stacks.ku_ring.data.mapper
+
+import com.ku_stacks.ku_ring.data.model.Push
+import com.ku_stacks.ku_ring.ui.my_notification.ui_model.PushContentUiModel
+import com.ku_stacks.ku_ring.ui.my_notification.ui_model.PushDataUiModel
+import com.ku_stacks.ku_ring.ui.my_notification.ui_model.PushDateHeaderUiModel
+
+fun List<Push>.toPushUiModelList(): List<PushDataUiModel> {
+    val pushDataList = ArrayList<PushDataUiModel>()
+    forEachIndexed { index, push ->
+        /** 두 알림 날짜를 비교해서 서로 다른 날짜면 PushDateHeaderUiModel 삽입 */
+        val isNewDay = if (index == 0) {
+            true
+        } else {
+            val prevItem = this[index - 1]
+            prevItem.postedDate != push.postedDate
+        }
+
+        if (isNewDay) {
+            pushDataList.add(PushDateHeaderUiModel(push.postedDate))
+        }
+        pushDataList.add(push.toPushContentUiModel())
+    }
+    return pushDataList
+}
+
+fun Push.toPushContentUiModel(): PushContentUiModel {
+    return PushContentUiModel(
+        articleId = articleId,
+        category = category,
+        postedDate = postedDate,
+        subject = subject,
+        baseUrl = baseUrl,
+        isNew = isNew,
+        receivedDate = receivedDate,
+        tag = tag
+    )
+}
