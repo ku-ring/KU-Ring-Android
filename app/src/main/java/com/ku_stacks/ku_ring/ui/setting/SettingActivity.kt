4082e2e7d461a14a8119f5c5ba4803f6e6110839
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-03-24 02:00:31 +0900

    [추가] SettingActivity에서 Layout 마다 클릭리스너 등록

--- app/src/main/java/com/ku_stacks/ku_ring/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/setting/SettingActivity.kt
@@ -1,9 +1,13 @@
 package com.ku_stacks.ku_ring.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.databinding.ActivitySettingBinding
+import com.ku_stacks.ku_ring.ui.feedback.FeedbackActivity
+import com.ku_stacks.ku_ring.ui.notion.NotionViewActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,6 +31,54 @@ class SettingActivity : AppCompatActivity() {
         binding.settingBackBt.setOnClickListener {
             finish()
             overridePendingTransition(R.anim.anim_slide_left_enter, R.anim.anim_slide_left_exit)
+        }
+
+        binding.subscribeLayout.subscribeNoticeLayout.setOnClickListener {
+            // TODO
+        }
+        binding.subscribeLayout.subscribeExtLayout.setOnClickListener {
+            // TODO
+        }
+
+        binding.informationLayout.newContentsLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_new_contents_url))
+            startActivity(intent)
+        }
+        binding.informationLayout.teamLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_kuring_team_url))
+            startActivity(intent)
+        }
+        binding.informationLayout.privacyPolicyLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_privacy_policy_url))
+            startActivity(intent)
+        }
+        binding.informationLayout.termsOfServiceLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_terms_of_service_url))
+            startActivity(intent)
+        }
+        binding.informationLayout.openSourceLayout.setOnClickListener {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+            OssLicensesMenuActivity.setActivityTitle(getString(R.string.open_source_license))
+        }
+
+        binding.snsLayout.instagramLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.instagram_url))
+            startActivity(intent)
+        }
+        binding.snsLayout.kakaoChannelLayout.setOnClickListener {
+            val intent = Intent(this, NotionViewActivity::class.java)
+            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.kakao_url))
+            startActivity(intent)
+        }
+
+        binding.feedbackLayout.feedbackSendLayout.setOnClickListener {
+            val intent = Intent(this, FeedbackActivity::class.java)
+            startActivity(intent)
         }
     }
 
