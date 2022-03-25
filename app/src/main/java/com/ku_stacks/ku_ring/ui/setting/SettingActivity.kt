fbf4c6cbe779fc371199ab1cb681e74dc50639ec
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-03-25 17:40:19 +0900

    [추가] SettingActivity에서 기타 알림 토글 버튼 추가, 설정 값은 SharedPreference에 저장

--- app/src/main/java/com/ku_stacks/ku_ring/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/setting/SettingActivity.kt
@@ -2,10 +2,12 @@ package com.ku_stacks.ku_ring.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.databinding.ActivitySettingBinding
+import com.ku_stacks.ku_ring.ui.edit_subscription.EditSubscriptionActivity
 import com.ku_stacks.ku_ring.ui.feedback.FeedbackActivity
 import com.ku_stacks.ku_ring.ui.notion.NotionViewActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,12 +16,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingBinding
+    private val viewModel by viewModels<SettingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupBinding()
         setupView()
+
+        binding.subscribeLayout.subscribeExtSwitch.isChecked = viewModel.isExtNotificationAllowed()
     }
 
     private fun setupBinding() {
@@ -33,36 +38,28 @@ class SettingActivity : AppCompatActivity() {
             overridePendingTransition(R.anim.anim_slide_left_enter, R.anim.anim_slide_left_exit)
         }
 
+        /** subscribe layout */
         binding.subscribeLayout.subscribeNoticeLayout.setOnClickListener {
-            // TODO
+            val intent = Intent(this, EditSubscriptionActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
         }
-        binding.subscribeLayout.subscribeExtLayout.setOnClickListener {
-            // TODO
+        binding.subscribeLayout.subscribeExtSwitch.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.setExtNotificationAllowed(isChecked)
         }
 
+        /** information layout */
         binding.informationLayout.newContentsLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_new_contents_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.notion_new_contents_url))
         }
         binding.informationLayout.teamLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_kuring_team_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.notion_kuring_team_url))
         }
         binding.informationLayout.privacyPolicyLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_privacy_policy_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.notion_privacy_policy_url))
         }
         binding.informationLayout.termsOfServiceLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.notion_terms_of_service_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.notion_terms_of_service_url))
         }
         binding.informationLayout.openSourceLayout.setOnClickListener {
             startActivity(Intent(this, OssLicensesMenuActivity::class.java))
@@ -70,24 +67,27 @@ class SettingActivity : AppCompatActivity() {
             overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
         }
 
+        /** sns layout */
         binding.snsLayout.instagramLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.instagram_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.instagram_url))
         }
         binding.snsLayout.kakaoChannelLayout.setOnClickListener {
-            val intent = Intent(this, NotionViewActivity::class.java)
-            intent.putExtra(NotionViewActivity.NOTION_URL, getString(R.string.kakao_url))
-            startActivity(intent)
-            overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
+            startWebViewActivity(getString(R.string.kakao_url))
         }
 
+        /** feedback layout */
         binding.feedbackLayout.feedbackSendLayout.setOnClickListener {
             val intent = Intent(this, FeedbackActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
         }
+    }
+
+    private fun startWebViewActivity(url: String) {
+        val intent = Intent(this, NotionViewActivity::class.java)
+        intent.putExtra(NotionViewActivity.NOTION_URL, url)
+        startActivity(intent)
+        overridePendingTransition(R.anim.anim_slide_right_enter, R.anim.anim_stay_exit)
     }
 
     override fun onBackPressed() {
