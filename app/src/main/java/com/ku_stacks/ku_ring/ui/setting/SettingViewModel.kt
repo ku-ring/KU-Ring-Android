0f5c3cbc28dfcce359e955d24144cce8b1cbe03e
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-03-25 17:40:19 +0900

    [추가] SettingActivity에서 기타 알림 토글 버튼 추가, 설정 값은 SharedPreference에 저장

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/setting/SettingViewModel.kt
@@ -0,0 +1,18 @@
+package com.ku_stacks.ku_ring.ui.setting
+
+import androidx.lifecycle.ViewModel
+import com.ku_stacks.ku_ring.util.PreferenceUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val pref: PreferenceUtil
+) : ViewModel() {
+
+    fun isExtNotificationAllowed() = pref.extNotificationAllowed
+
+    fun setExtNotificationAllowed(value: Boolean) {
+        pref.extNotificationAllowed = value
+    }
+}
