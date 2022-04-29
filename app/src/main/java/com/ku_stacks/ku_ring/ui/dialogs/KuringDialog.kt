64dd1964cda8cd2b6faef49f4276db37cc59477c
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-04-29 23:56:42 +0900

    [추가] KuringDialog 추가

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/dialogs/KuringDialog.kt
@@ -0,0 +1,67 @@
+package com.ku_stacks.ku_ring.ui.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.ku_stacks.ku_ring.R
+import com.ku_stacks.ku_ring.adapter.visibleIf
+
+class KuringDialog(context: Context) : Dialog(context) {
+
+    private var title: String? = null
+    private var description: String? = null
+
+    private lateinit var titleTextView: TextView
+    private lateinit var descriptionTextView: TextView
+    private lateinit var cancelButton: TextView
+    private lateinit var confirmButton: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        setContentView(R.layout.dialog_kuring)
+
+        titleTextView = findViewById(R.id.title_txt)
+        descriptionTextView = findViewById(R.id.description_txt)
+        cancelButton = findViewById(R.id.dialog_cancel_bt)
+        confirmButton = findViewById(R.id.dialog_confirm_bt)
+
+        cancelButton.setOnClickListener {
+            dismiss()
+        }
+
+        confirmButton.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    fun setText(_title: String?, _description: String?) = this.apply {
+        title = _title
+        description = _description
+    }
+
+    fun setOnConfirmClickListener(onClickListener: View.OnClickListener) = this.apply {
+        confirmButton.setOnClickListener { v ->
+            onClickListener.onClick(v)
+            dismiss()
+        }
+    }
+
+    override fun show() {
+        super.show()
+        title?.let {
+            titleTextView.text = it
+        }
+        description?.let {
+            descriptionTextView.text = it
+        }
+
+        titleTextView.visibleIf(title != null)
+        descriptionTextView.visibleIf(description != null)
+    }
+}
--- app/src/main/java/com/ku_stacks/ku_ring/util/ContextExt.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/util/ContextExt.kt
@@ -4,9 +4,17 @@ import android.content.Context
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import com.ku_stacks.ku_ring.ui.dialogs.KuringDialog
 
 fun View.showSnackBar(msg: String) =
     Snackbar.make(this, msg, Snackbar.LENGTH_SHORT).show()
 
 fun Context.showToast(msg: String) =
     Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+
+fun Context.makeDialog(title: String?, description: String?): KuringDialog {
+    val dialog = KuringDialog(this)
+        .setText(_title = title, _description = description)
+    dialog.show()
+    return dialog
+}
