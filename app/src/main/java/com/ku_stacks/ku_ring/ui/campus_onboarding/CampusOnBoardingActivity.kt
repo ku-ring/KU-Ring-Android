b15e824dc6c2620c52bf0adc05312c06a5e47f6c
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-05-03 01:31:06 +0900

    [추가] CampusOnBoardingActivity에 ViewPager2 인디케이터 추가

--- app/src/main/java/com/ku_stacks/ku_ring/ui/campus_onboarding/CampusOnBoardingActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/campus_onboarding/CampusOnBoardingActivity.kt
@@ -1,16 +1,23 @@
 package com.ku_stacks.ku_ring.ui.campus_onboarding
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.ku_stacks.ku_ring.databinding.ActivityCampusOnboardingBinding
+import timber.log.Timber
 
 class CampusOnBoardingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCampusOnboardingBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            binding.indicatorView.selection = position
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setupBinding()
         setupFragment()
@@ -22,7 +29,19 @@ class CampusOnBoardingActivity : AppCompatActivity() {
     }
 
     private fun setupFragment() {
-        val pagerAdapter = CampusOnBoardingPagerAdapter(supportFragmentManager, lifecycle)
-        binding.viewpager.adapter = pagerAdapter
+        binding.viewpager.apply {
+            adapter = CampusOnBoardingPagerAdapter(supportFragmentManager, lifecycle)
+        }
+
+        binding.indicatorView.apply {
+            count = 2
+        }
+
+        binding.viewpager.registerOnPageChangeCallback(pageChangeCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.viewpager.unregisterOnPageChangeCallback(pageChangeCallback)
     }
 }
