7e3278709f0225e6cb5eb0cbc9a58353b9a03d2a
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-05-08 17:23:49 +0900

    [추가] activity_chat.xml 추가

--- app/src/main/java/com/ku_stacks/ku_ring/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/chat/ChatActivity.kt
@@ -1,6 +1,32 @@
 package com.ku_stacks.ku_ring.ui.chat
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ku_stacks.ku_ring.R
+import com.ku_stacks.ku_ring.databinding.ActivityChatBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupBinding()
+        setupView()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_chat)
+        binding.lifecycleOwner = this
+    }
+
+    private fun setupView() {
+        binding.chatBackBt.setOnClickListener {
+            finish()
+        }
+    }
 }
---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/chat/ChatViewModel.kt
@@ -0,0 +1,11 @@
+package com.ku_stacks.ku_ring.ui.chat
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+
+) : ViewModel() {
+}
