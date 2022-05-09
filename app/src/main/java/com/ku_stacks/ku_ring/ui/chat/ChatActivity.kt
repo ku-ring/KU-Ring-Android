d2d8b0020f55430db3e7ac322e021e9128ad54af
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-05-09 21:25:50 +0900

    [디자인 수정] ChatActivity 전송버튼 tint, adminMessage 패딩 수정

--- app/src/main/java/com/ku_stacks/ku_ring/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/chat/ChatActivity.kt
@@ -1,14 +1,20 @@
 package com.ku_stacks.ku_ring.ui.chat
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.databinding.DataBindingUtil
 import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.databinding.ActivityChatBinding
 import com.ku_stacks.ku_ring.util.makeDialog
 import com.ku_stacks.ku_ring.util.modified_external_library.RecyclerViewPager
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class ChatActivity : AppCompatActivity() {
@@ -43,6 +49,18 @@ class ChatActivity : AppCompatActivity() {
             binding.chatMessageEt.text.clear()
             viewModel.sendMessage(message)
         }
+        binding.chatMessageEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    // modify tint color
+                    ImageViewCompat.setImageTintList(binding.chatSendBt, ColorStateList.valueOf(getColor(R.color.kus_green_50)))
+                } else {
+                    ImageViewCompat.setImageTintList(binding.chatSendBt, ColorStateList.valueOf(getColor(R.color.kus_green)))
+                }
+            }
+        })
     }
 
     private fun setupRecyclerView() {
