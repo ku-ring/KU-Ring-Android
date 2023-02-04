0d58bd3c0402409291897b7379d6f10b4c28e29f
Wooyoung Myung <mwy3055@gmail.com>
2023-02-04 13:33:56 +0900

    [추가] 보관된 공지 model class 추가

---
+++ app/src/main/java/com/ku_stacks/ku_ring/data/model/SavedNotice.kt
@@ -0,0 +1,9 @@
+package com.ku_stacks.ku_ring.data.model
+
+data class SavedNotice(
+    val articleId: String,
+    val category: String,
+    val baseUrl: String,
+    val postedDate: String,
+    val subject: String,
+)
