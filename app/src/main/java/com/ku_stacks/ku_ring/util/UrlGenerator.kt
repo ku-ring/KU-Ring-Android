29fd65a2caf03c8143a21a3ce8d345eda8a46762
Wooyoung Myung <mwy3055@gmail.com>
2023-03-04 14:33:50 +0900

    [수정] UrlGenerator에서 category를 영어로만 받도록 수정

--- app/src/main/java/com/ku_stacks/ku_ring/util/UrlGenerator.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/util/UrlGenerator.kt
@@ -1,10 +1,16 @@
 package com.ku_stacks.ku_ring.util
 
+import timber.log.Timber
+
 object UrlGenerator {
 
     @JvmStatic
     fun generateNoticeUrl(articleId: String, category: String, baseUrl: String): String {
-        return if (category == "도서관") {
+        if (!category.isOnlyAlphabets()) {
+            Timber.e("Only english category should be given: $category")
+        }
+
+        return if (category == "library") {
             "$baseUrl/$articleId"
         } else {
             "$baseUrl?id=$articleId"
