599c028f9bf93a9cd5db67fb24118d8a2f41f90d
Wooyoung Myung <mwy3055@gmail.com>
2023-03-12 22:37:35 +0900

    [수정] 카테고리별 Fragment가 아닌 HomeBaseFragment를 생성하도록 수정

--- app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/CategoryPagerAdapter.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/CategoryPagerAdapter.kt
@@ -4,14 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage0_Bachelor.BachelorFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage1_Scholarship.ScholarshipFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage2_employ.EmployFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage3_nation.NationFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage4_Student.StudentFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage5_Industry.IndustryFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage6_Nornal.NormalFragment
-import com.ku_stacks.ku_ring.ui.main.notice.category.stage7_Library.LibraryFragment
+import com.ku_stacks.ku_ring.ui.main.notice.category.HomeBaseFragment
 
 class CategoryPagerAdapter(fm: FragmentManager, lc: Lifecycle) : FragmentStateAdapter(fm, lc) {
 
@@ -19,16 +12,8 @@ class CategoryPagerAdapter(fm: FragmentManager, lc: Lifecycle) : FragmentStateAd
      * array 의 내부를 lambda 로 하지않으면 memory leak 발생
      * (Fragment 가 destroy 될 때 참조가 남음)
      */
-    private val items = arrayOf(
-        { BachelorFragment() },
-        { ScholarshipFragment() },
-        { EmployFragment() },
-        { NationFragment() },
-        { StudentFragment() },
-        { IndustryFragment() },
-        { NormalFragment() },
-        { LibraryFragment() }
-    )
+    private val items = listOf("bch", "sch", "emp", "nat", "stu", "ind", "nor", "lib")
+        .map { shortCategory -> { HomeBaseFragment(shortCategory) } }
 
     override fun getItemCount() = items.size
 
