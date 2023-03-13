41067dd351d5e83603ea4aa908f6317b4d679ff3
Wooyoung Myung <mwy3055@gmail.com>
2023-03-13 21:49:11 +0900

    [수정] 변경된 코드에 맞게 주석 수정

--- app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/NoticesPagerAdapter.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/NoticesPagerAdapter.kt
@@ -9,7 +9,7 @@ import com.ku_stacks.ku_ring.ui.main.notice.category.NoticesChildFragment
 class NoticesPagerAdapter(fm: FragmentManager, lc: Lifecycle) : FragmentStateAdapter(fm, lc) {
 
     /**
-     * array 의 내부를 lambda 로 하지않으면 memory leak 발생
+     * lambda로 하지 않으면 memory leak 발생
      * (Fragment 가 destroy 될 때 참조가 남음)
      */
     private val items = listOf("bch", "sch", "emp", "nat", "stu", "ind", "nor", "lib")
