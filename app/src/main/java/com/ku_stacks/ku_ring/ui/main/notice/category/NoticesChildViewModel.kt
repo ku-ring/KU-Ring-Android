ec903e86048722c00245bca920d8c26133329416
Wooyoung Myung <mwy3055@gmail.com>
2023-03-18 16:49:23 +0900

    [추가] 카테고리 fragment가 사용할 ViewModel 정의

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/category/NoticesChildViewModel.kt
@@ -0,0 +1,44 @@
+package com.ku_stacks.ku_ring.ui.main.notice.category
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.ku_stacks.ku_ring.data.model.Notice
+import com.ku_stacks.ku_ring.repository.NoticeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class NoticesChildViewModel @Inject constructor(
+    private val noticeRepository: NoticeRepository,
+) : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+
+    fun insertNoticeAsOld(notice: Notice) {
+        disposable.add(
+            noticeRepository.insertNoticeAsOld(notice)
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    //Timber.e("noticeRecord Insert true : $articleId")
+                }, { Timber.e("noticeRecord Insert fail $it") })
+        )
+    }
+
+    fun getNotices(shortCategory: String): Flowable<PagingData<Notice>> =
+        noticeRepository.getNotices(shortCategory, viewModelScope)
+
+    override fun onCleared() {
+        super.onCleared()
+
+        if (!disposable.isDisposed) {
+            disposable.dispose()
+            Timber.e("compositeDisposable disposed")
+        }
+    }
+
+}
