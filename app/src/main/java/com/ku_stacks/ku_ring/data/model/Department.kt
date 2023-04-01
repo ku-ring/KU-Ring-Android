b316f1fecc143ab68f7f1ec35b4fba51f82d2a64
Wooyoung Myung <mwy3055@gmail.com>
2023-04-01 16:50:40 +0900

    [추가] 학과를 나타내는 model 클래스 추가

---
+++ app/src/main/java/com/ku_stacks/ku_ring/data/model/Department.kt
@@ -0,0 +1,9 @@
+package com.ku_stacks.ku_ring.data.model
+
+data class Department(
+    val name: String,
+    val shortName: String,
+    val koreanName: String,
+    val isSubscribed: Boolean,
+    val isSelected: Boolean,
+)
