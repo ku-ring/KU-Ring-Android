8ce9d2e5b50fe5e006cb00bba42383efc069344c
Wooyoung Myung <mwy3055@gmail.com>
2023-05-02 16:35:12 +0900

    [추가] PageKeyDao 페이징 key 테스트 코드 추가

---
+++ app/src/test/java/com/ku_stacks/ku_ring/persistence/PageKeyDaoTest.kt
@@ -0,0 +1,40 @@
+package com.ku_stacks.ku_ring.persistence
+
+import com.ku_stacks.ku_ring.data.db.PageKeyDao
+import com.ku_stacks.ku_ring.data.db.PageKeyEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [23])
+class PageKeyDaoTest : LocalDbAbstract() {
+
+    private lateinit var pageKeyDao: PageKeyDao
+
+    @Before
+    fun init() {
+        pageKeyDao = db.pageKeyDao()
+    }
+
+    @Test
+    fun `insert and get PageKey Test`() = runTest {
+        // given
+        val pageKeyEntity = PageKeyEntity(articleId = "123456", page = 1)
+        pageKeyDao.insertPageKeys(listOf(pageKeyEntity))
+
+        // when
+        val storedEntity = pageKeyDao.getPageKey(pageKeyEntity.articleId)
+
+        // then
+        assertThat(pageKeyEntity, `is`(storedEntity))
+    }
+
+}
