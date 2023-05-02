829c6277841afea601d7b7e1239501654559ee7e
yeon-kyu <cus1128@naver.com>
2023-05-03 01:56:06 +0900

    [수정] robolectric 버전 변경

--- app/src/test/java/com/ku_stacks/ku_ring/persistence/PushDaoTest.kt
+++ app/src/test/java/com/ku_stacks/ku_ring/persistence/PushDaoTest.kt
@@ -12,7 +12,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [23])
+@Config(sdk = [24])
 class PushDaoTest : LocalDbAbstract() {
 
     private lateinit var pushDao: PushDao
