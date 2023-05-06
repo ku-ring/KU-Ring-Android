e3a36434d1cd4de18fd2c541b5744a60fb4ceb1f
Wooyoung Myung <mwy3055@gmail.com>
2023-05-06 22:21:30 +0900

    [수정] repository 종류 변경

--- app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/department/DepartmentNoticeViewModel.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/department/DepartmentNoticeViewModel.kt
@@ -3,21 +3,21 @@ package com.ku_stacks.ku_ring.ui.main.notice.department
 import androidx.lifecycle.ViewModel
 import androidx.paging.PagingData
 import com.ku_stacks.ku_ring.data.model.Notice
-import com.ku_stacks.ku_ring.repository.NoticeRepository
+import com.ku_stacks.ku_ring.repository.DepartmentNoticeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class DepartmentNoticeViewModel @Inject constructor(
-    private val noticeRepository: NoticeRepository
+    private val departmentNoticeRepository: DepartmentNoticeRepository
 ) : ViewModel() {
 
     private val currentNotices: HashMap<String, Flow<PagingData<Notice>>> = hashMapOf()
 
     fun getDepartmentNotices(shortName: String): Flow<PagingData<Notice>> {
         if (shortName !in currentNotices) {
-            currentNotices[shortName] = noticeRepository.getDepartmentNotices(shortName)
+            currentNotices[shortName] = departmentNoticeRepository.getDepartmentNotices(shortName)
         }
         return currentNotices[shortName]!!
     }
