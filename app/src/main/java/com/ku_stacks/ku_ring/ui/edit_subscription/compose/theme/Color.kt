718fccf08a19535737e34d01433d734b70a56adb
Wooyoung Myung <mwy3055@gmail.com>
2023-05-12 12:32:02 +0900

    [추가] 앱에서 사용할 Theme 추가

---
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/edit_subscription/compose/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.ku_stacks.ku_ring.ui.edit_subscription.compose.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.ku_stacks.ku_ring.R
+
+private val KuringGreen: Color
+    @Composable get() = colorResource(R.color.kus_green)
+private val KuringGreen50: Color
+    @Composable get() = colorResource(R.color.kus_green_50)
+private val KuringSecondaryGreen: Color
+    @Composable get() = colorResource(R.color.kus_secondary_green)
+
+val colorPalette: Colors
+    @Composable get() = lightColors(
+        primary = KuringGreen,
+        primaryVariant = KuringGreen50,
+        secondary = KuringSecondaryGreen,
+    )
