9ca8b6a4faff6e5d869d9bb2da94e89063a21070
Wooyoung Myung <mwy3055@gmail.com>
2023-05-13 14:51:53 +0900

    [수정] MutableMap(또는 Set)을 수정할 때 exception이 발생하면 즉시 종료하도록 수정

--- app/src/main/java/com/ku_stacks/ku_ring/util/StateFlowUtil.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/util/StateFlowUtil.kt
@@ -4,13 +4,21 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import timber.log.Timber
 
 fun <K, V> MutableStateFlow<MutableMap<K, V>>.modifyMap(block: MutableMap<K, V>.() -> Unit) {
-    val newValue = this.value.toMutableMap().apply { block() }
+    val newValue = try {
+        this.value.toMutableMap().apply { block() }
+    } catch (e: Exception) {
+        return
+    }
     Timber.d("before: $value, after: $newValue")
     this.value = newValue
 }
 
 fun <E> MutableStateFlow<MutableSet<E>>.modifySet(block: MutableSet<E>.() -> Unit) {
-    val newValue = this.value.toMutableSet().apply { block() }
+    val newValue = try {
+        this.value.toMutableSet().apply { block() }
+    } catch (e: Exception) {
+        return
+    }
     Timber.d("before: $value, after: $newValue")
     this.value = newValue
 }
