dad27a722414230edff0040885504a1d702281e3
Wooyoung Myung <mwy3055@gmail.com>
2023-05-14 19:51:54 +0900

    [추가] 학과별 공지 composable을 fragment와 연결

--- app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/department/DepartmentNoticeFragment.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/notice/department/DepartmentNoticeFragment.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.ku_stacks.ku_ring.R
+import com.ku_stacks.ku_ring.data.model.Notice
 import com.ku_stacks.ku_ring.databinding.FragmentDepartmentNoticeBinding
 import com.ku_stacks.ku_ring.ui.compose.theme.KuringTheme
-import com.ku_stacks.ku_ring.ui.main.notice.category.NoticePagingAdapter
+import com.ku_stacks.ku_ring.ui.main.notice.department.compose.DepartmentNoticeScreen
+import com.ku_stacks.ku_ring.ui.notice_webview.NoticeWebActivity
 import com.ku_stacks.ku_ring.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -50,12 +66,49 @@ class DepartmentNoticeFragment : Fragment() {
         }
     }
 
+    @OptIn(ExperimentalMaterialApi::class)
     private fun setupNoticeListView() {
         binding.departmentNotices.setContent {
             KuringTheme {
-                Text("여기에 학과 선택 및 공지 리스트가 들어갑니다.")
+                val selectedDepartments by viewModel.subscribedDepartments.collectAsState()
+                val noticesFlow by viewModel.currentDepartmentNotice.collectAsState()
+                val notices = noticesFlow.collectAsLazyPagingItems()
+
+                var isRefreshing by remember { mutableStateOf(false) }
+                val refreshState = rememberPullRefreshState(
+                    refreshing = isRefreshing,
+                    onRefresh = {
+                        isRefreshing = true
+                        notices.refresh()
+                        isRefreshing = false
+                    },
+                    refreshThreshold = 100.dp,
+                )
+                DepartmentNoticeScreen(
+                    selectedDepartments = selectedDepartments,
+                    onSelectDepartment = viewModel::selectDepartment,
+                    notices = notices,
+                    onNoticeClick = ::startNoticeActivity,
+                    onFabClick = {},
+                    isRefreshing = isRefreshing,
+                    refreshState = refreshState,
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.kus_background))
+                        .padding(bottom = 56.dp)
+                        .fillMaxSize(),
+                    scope = rememberCoroutineScope(),
+                )
             }
         }
+    }
+
+    private fun startNoticeActivity(notice: Notice) {
+        val intent = NoticeWebActivity.createIntent(requireActivity(), notice)
+        startActivity(intent)
+        requireActivity().overridePendingTransition(
+            R.anim.anim_slide_right_enter,
+            R.anim.anim_stay_exit
+        )
     }
 
     private fun observeLoadingState() {
