1bc908b8a2337aacdec83a4761b7b5d6ef45061e
Wooyoung Myung <mwy3055@gmail.com>
2023-05-15 18:43:05 +0900

    [수정] 검색 response는 아직 depth가 1 줄어들지 않음

--- app/src/main/java/com/ku_stacks/ku_ring/data/api/response/SearchNoticeListResponse.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/data/api/response/SearchNoticeListResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class SearchNoticeListResponse(
     @SerializedName("code") val code: Int,
     @SerializedName("message") val message: String,
-    @SerializedName("data") val data: List<SearchNoticeResponse>?,
+    @SerializedName("data") val data: SearchNoticeDataResponse?,
 ) {
     val isSuccess: Boolean
         get() = (code == 200)
--- app/src/main/java/com/ku_stacks/ku_ring/data/api/response/SearchStaffListResponse.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/data/api/response/SearchStaffListResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class SearchStaffListResponse(
     @SerializedName("code") val code: Int,
     @SerializedName("message") val message: String,
-    @SerializedName("data") val data: List<SearchStaffResponse>?,
+    @SerializedName("data") val data: SearchStaffDataResponse?,
 ) {
     val isSuccess: Boolean
         get() = (code == 200)
