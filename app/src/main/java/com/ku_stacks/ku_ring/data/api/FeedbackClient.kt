90f505048c6734b52b363a30f25f3c9ea34fd22d
Wooyoung Myung <mwy3055@gmail.com>
2023-07-02 14:51:58 +0900

    [수정] 피드백 Request와 Body를 v2 스키마로 수정

--- app/src/main/java/com/ku_stacks/ku_ring/data/api/FeedbackClient.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/data/api/FeedbackClient.kt
@@ -1,7 +1,7 @@
 package com.ku_stacks.ku_ring.data.api
 
 import com.ku_stacks.ku_ring.data.api.request.FeedbackRequest
-import com.ku_stacks.ku_ring.data.api.response.DefaultResponse
+import com.ku_stacks.ku_ring.data.api.response.DefaultV2Response
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -9,6 +9,7 @@ class FeedbackClient @Inject constructor(
     private val feedbackService: FeedbackService
 ) {
     fun sendFeedback(
-        feedbackRequest: FeedbackRequest
-    ): Single<DefaultResponse> = feedbackService.sendFeedback(feedbackRequest)
+        token: String,
+        feedbackRequest: FeedbackRequest,
+    ): Single<DefaultV2Response> = feedbackService.sendFeedback(token, feedbackRequest)
 }
--- app/src/main/java/com/ku_stacks/ku_ring/data/api/FeedbackService.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/data/api/FeedbackService.kt
@@ -1,14 +1,16 @@
 package com.ku_stacks.ku_ring.data.api
 
 import com.ku_stacks.ku_ring.data.api.request.FeedbackRequest
-import com.ku_stacks.ku_ring.data.api.response.DefaultResponse
+import com.ku_stacks.ku_ring.data.api.response.DefaultV2Response
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface FeedbackService {
-    @POST("v1/feedback")
+    @POST("v2/users/feedbacks")
     fun sendFeedback(
-        @Body feedbackRequest: FeedbackRequest
-    ): Single<DefaultResponse>
+        @Header("User-Token") token: String,
+        @Body feedbackRequest: FeedbackRequest,
+    ): Single<DefaultV2Response>
 }
