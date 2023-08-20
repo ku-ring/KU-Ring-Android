d69c73b09fbe9652ae8209636cde5f6c8722c646
Wooyoung Myung <mwy3055@gmail.com>
2023-08-21 00:19:48 +0900

    kuring-62 [수정] 테스트 코드 수정

--- app/src/test/java/com/ku_stacks/ku_ring/util/DateUtilTest.kt
+++ app/src/test/java/com/ku_stacks/ku_ring/util/DateUtilTest.kt
@@ -1,7 +1,8 @@
 package com.ku_stacks.ku_ring.util
 
 import junit.framework.TestCase.assertEquals
-import org.junit.Assert.assertThrows
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Test
 import java.util.Calendar
 
@@ -11,10 +12,10 @@ class DateUtilTest {
         val current = Calendar.getInstance()
         val actual = DateUtil.getCalendar(0, 0, 0, 0)
 
-        assertEquals(current.get(Calendar.DAY_OF_MONTH), actual.get(Calendar.DAY_OF_MONTH))
-        assertEquals(0, actual.get(Calendar.HOUR_OF_DAY))
-        assertEquals(0, actual.get(Calendar.MINUTE))
-        assertEquals(0, actual.get(Calendar.SECOND))
+        assertEquals(current.get(Calendar.DAY_OF_MONTH), actual?.get(Calendar.DAY_OF_MONTH))
+        assertEquals(0, actual?.get(Calendar.HOUR_OF_DAY))
+        assertEquals(0, actual?.get(Calendar.MINUTE))
+        assertEquals(0, actual?.get(Calendar.SECOND))
     }
 
     @Test
@@ -22,7 +23,9 @@ class DateUtilTest {
         val randomTime = DateUtil.getCalendar(0, 1, 10, 16)
         val after1Second = DateUtil.getCalendar(0, 1, 10, 17)
 
-        val timeDiff = after1Second.timeInMillis - randomTime.timeInMillis
+        assertNotNull(randomTime)
+        assertNotNull(after1Second)
+        val timeDiff = after1Second!!.timeInMillis - randomTime!!.timeInMillis
         assertEquals(1000L, timeDiff)
     }
 
@@ -31,14 +34,14 @@ class DateUtilTest {
         val midnight = DateUtil.getCalendar(0, 0, 0, 0)
         val am1 = DateUtil.getCalendar(0, 1, 0, 0)
 
-        val timeDiff = am1.timeInMillis - midnight.timeInMillis
+        assertNotNull(midnight)
+        assertNotNull(am1)
+        val timeDiff = am1!!.timeInMillis - midnight!!.timeInMillis
         assertEquals(60 * 60 * 1000L, timeDiff)
     }
 
     @Test
     fun timeDiff_illegalHourArgument() {
-        assertThrows(IllegalArgumentException::class.java) {
-            val actual = DateUtil.getCalendar(0, 27, 0, 1)
-        }
+        assertNull(DateUtil.getCalendar(0, 27, 0, 1))
     }
 }
