b9809b6b37f4fef2c6dd8ed4a3d7adf8026c5ede
yeon-kyu <cus1128@naver.com>
2023-08-30 23:30:21 +0900

    kuring-73 FeatureTest 모듈 추가

---
+++ TestFeature/src/main/java/com/ku_stacks/testfeature/TestFeatureActivity.kt
@@ -0,0 +1,11 @@
+package com.ku_stacks.testfeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestFeatureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testfeature)
+    }
+}
