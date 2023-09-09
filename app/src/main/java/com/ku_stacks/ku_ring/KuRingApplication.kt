2d3fa8036aa435b5f812471ff08a9ef15f37b679
Wooyoung Myung <mwy3055@gmail.com>
2023-09-09 13:49:14 +0900

    kuring-76 [수정] Android 기본 WorkerFactory 대신 커스텀 팩토리를 사용하도록 수정

--- app/src/main/java/com/ku_stacks/ku_ring/KuRingApplication.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/KuRingApplication.kt
@@ -1,7 +1,21 @@
 package com.ku_stacks.ku_ring
 
 import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import com.ku_stacks.ku_ring.navigator.KuringNavigator
+import com.ku_stacks.ku_ring.work.ReEngagementNotificationWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class KuRingApplication : Application()
+class KuRingApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var navigator: KuringNavigator
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(Log.INFO)
+            .setWorkerFactory(ReEngagementNotificationWorkerFactory(navigator::createMainIntent))
+            .build()
+    }
+}
