bf0544f58cfc024f1d2f952d6f029de566902bf6
Wooyoung Myung <mwy3055@gmail.com>
2023-10-03 20:22:39 +0900

    kuring-69 [추가] :data:domain에 선언되어 있던 mock() 함수를 DomainFixtures로 옮김

--- data/domain/src/main/java/com/ku_stacks/ku_ring/domain/Department.kt
+++ data/domain/src/main/java/com/ku_stacks/ku_ring/domain/Department.kt
@@ -7,15 +7,4 @@ data class Department(
     val isSubscribed: Boolean,
     val isSelected: Boolean,
     val isNotificationEnabled: Boolean,
-) {
-    companion object {
-        fun mock() = Department(
-            name = "smart_ict_convergence",
-            shortName = "sicte",
-            koreanName = "스마트ICT융합공학과",
-            isSubscribed = false,
-            isSelected = false,
-            isNotificationEnabled = false,
-        )
-    }
-}
+)
--- data/domain/src/main/java/com/ku_stacks/ku_ring/domain/Notice.kt
+++ data/domain/src/main/java/com/ku_stacks/ku_ring/domain/Notice.kt
@@ -13,20 +13,4 @@ data class Notice(
     val isSaved: Boolean,
     val isReadOnStorage: Boolean,
     val tag: List<String>
-) {
-    companion object {
-        fun mock() = Notice(
-            postedDate = "20220203",
-            subject = "2022학년도 1학기 재입학 합격자 유의사항 안내",
-            category = "bachelor",
-            url = "https://www.konkuk.ac.kr/do/MessageBoard/ArticleRead.do?id=5b4a11b",
-            articleId = "5b4a11b",
-            isNew = true,
-            isRead = false,
-            isSubscribing = false,
-            isSaved = false,
-            isReadOnStorage = false,
-            tag = emptyList()
-        )
-    }
-}
+)
