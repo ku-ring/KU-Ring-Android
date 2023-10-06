e9a514e8cdf230cc73dbb208ae726f0149d7d0ea
Wooyoung Myung <mwy3055@gmail.com>
2023-10-06 14:15:15 +0900

    kuring-69 [추가] UserRepository에 피드백 전송 함수 추가

--- data/user/src/main/java/com/ku_stacks/ku_ring/user/repository/UserRepositoryImpl.kt
+++ data/user/src/main/java/com/ku_stacks/ku_ring/user/repository/UserRepositoryImpl.kt
@@ -2,12 +2,18 @@ package com.ku_stacks.ku_ring.user.repository
 
 import com.ku_stacks.ku_ring.local.entity.BlackUserEntity
 import com.ku_stacks.ku_ring.local.room.BlackUserDao
+import com.ku_stacks.ku_ring.preferences.PreferenceUtil
+import com.ku_stacks.ku_ring.remote.user.FeedbackClient
+import com.ku_stacks.ku_ring.remote.user.request.FeedbackRequest
+import com.ku_stacks.ku_ring.remote.util.DefaultResponse
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val dao: BlackUserDao
+    private val dao: BlackUserDao,
+    private val feedbackClient: FeedbackClient,
+    private val pref: PreferenceUtil,
 ) : UserRepository {
     override fun blockUser(userId: String, nickname: String): Completable {
         return dao.blockUser(
@@ -24,5 +30,12 @@ class UserRepositoryImpl @Inject constructor(
             .map { blackList ->
                 blackList.map { user -> user.userId }
             }
+    }
+
+    override fun sendFeedback(feedback: String): Single<DefaultResponse> {
+        return feedbackClient.sendFeedback(
+            token = pref.fcmToken ?: "",
+            feedbackRequest = FeedbackRequest(feedback)
+        )
     }
 }
