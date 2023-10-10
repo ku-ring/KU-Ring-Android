0398fc4092d1a1078e94f908a6c61b9ea8b1e49c
Wooyoung Myung <mwy3055@gmail.com>
2023-10-10 21:28:33 +0900

    kuring-69 [수정] Repository를 @Provides 대신 @Binds로 inject

--- data/notice/src/main/java/com/ku_stacks/ku_ring/notice/di/RepositoryModule.kt
+++ data/notice/src/main/java/com/ku_stacks/ku_ring/notice/di/RepositoryModule.kt
@@ -1,29 +1,17 @@
 package com.ku_stacks.ku_ring.notice.di
 
-import com.ku_stacks.ku_ring.local.room.NoticeDao
 import com.ku_stacks.ku_ring.notice.repository.NoticeRepository
 import com.ku_stacks.ku_ring.notice.repository.NoticeRepositoryImpl
-import com.ku_stacks.ku_ring.preferences.PreferenceUtil
-import com.ku_stacks.ku_ring.remote.notice.NoticeClient
-import com.ku_stacks.ku_ring.util.IODispatcher
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-    @Provides
+abstract class RepositoryModule {
+    @Binds
     @Singleton
-    fun provideNoticeRepository(
-        noticeClient: NoticeClient,
-        noticeDao: NoticeDao,
-        pref: PreferenceUtil,
-        @IODispatcher ioDispatcher: CoroutineDispatcher,
-    ): NoticeRepository {
-        return NoticeRepositoryImpl(noticeClient, noticeDao, pref, ioDispatcher)
-    }
+    abstract fun provideNoticeRepository(repositoryImpl: NoticeRepositoryImpl): NoticeRepository
 }
