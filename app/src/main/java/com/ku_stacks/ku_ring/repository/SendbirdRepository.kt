01220ca388a8413b1c324f0df786a7bbd5132065
Wooyoung Myung <mwy3055@gmail.com>
2023-11-05 23:57:01 +0900

    kuring-91 [제거] sendbird 코드 제거

--- app/src/main/java/com/ku_stacks/ku_ring/repository/SendbirdRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.ku_stacks.ku_ring.repository
-
-import io.reactivex.rxjava3.core.Single
-
-interface SendbirdRepository {
-    fun hasDuplicateNickname(nickname: String, userId: String): Single<Boolean>
-}
