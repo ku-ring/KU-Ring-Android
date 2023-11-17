95e1893db8ad0421cffd5bd96b5395aa6fc9d9f0
yeon-kyu <cus1128@naver.com>
2023-11-17 23:16:56 +0900

    kuring-93 [변경] 쿠링 캠퍼스 화면 -> 건대 교지 페이지

--- app/src/main/java/com/ku_stacks/ku_ring/ui/main/campus_onboarding/CampusFragment.kt
+++ app/src/main/java/com/ku_stacks/ku_ring/ui/main/campus_onboarding/CampusFragment.kt
@@ -1,31 +1,76 @@
 package com.ku_stacks.ku_ring.ui.main.campus_onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import com.ku_stacks.ku_ring.databinding.FragmentCampusBinding
+import com.ku_stacks.ku_ring.databinding.FragmentKonkukArticleBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class CampusFragment : Fragment() {
 
-    private var _binding: FragmentCampusBinding? = null
-    private val binding
-        get() = _binding!!
+    private lateinit var binding: FragmentKonkukArticleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCampusBinding.inflate(inflater, container, false)
+        binding = FragmentKonkukArticleBinding.inflate(inflater)
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = request.url
+                Timber.e("request url : ${request.url}")
+                startActivity(intent)
+                return true
+            }
+        }
+
+        binding.webView.settings.apply {
+            builtInZoomControls = true
+            domStorageEnabled = true
+            javaScriptEnabled = true
+            loadWithOverviewMode = true
+            setSupportZoom(true)
+            displayZoomControls = false
+        }
+
+        // WebChromeClient
+        binding.webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                binding.progressbar.progress = newProgress
+                if (newProgress == 100) {
+                    binding.progressbar.visibility = View.GONE
+                    binding.webView.webChromeClient = null
+                } else {
+                    binding.progressbar.visibility = View.VISIBLE
+                }
+                super.onProgressChanged(view, newProgress)
+            }
+        }
+
+        binding.webView.loadUrl(KONKUK_ARTICLE_URL)
+    }
+
+    companion object {
+        private const val KONKUK_ARTICLE_URL = "https://m.blog.naver.com/kukyogi1984?categoryNo=73"
     }
 }
