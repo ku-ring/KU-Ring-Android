91176d333114f4c80fe9e94a82e1f30d2859ebac
Wooyoung Myung <mwy3055@gmail.com>
2023-11-18 18:07:52 +0900

    kuring-71 [수정] 온보딩 화면 코드를 모듈로 옮김

--- feature/onboarding/src/main/java/com/ku_stacks/ku_ring/onboarding/OnboardingActivity.kt
+++ feature/onboarding/src/main/java/com/ku_stacks/ku_ring/onboarding/OnboardingActivity.kt
@@ -1,4 +1,4 @@
-package com.ku_stacks.ku_ring.ui.onboarding
+package com.ku_stacks.ku_ring.onboarding
 
 import android.app.Activity
 import android.content.Intent
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.ku_stacks.ku_ring.R
 import com.ku_stacks.ku_ring.preferences.PreferenceUtil
 import com.ku_stacks.ku_ring.thirdparty.firebase.analytics.EventAnalytics
 import com.ku_stacks.ku_ring.ui_util.KuringNavigator
