de8467f3dcffad5cd485b6403df6d91c58a5d2c3
Wooyoung Myung <mwy3055@gmail.com>
2024-01-05 21:47:26 +0900

    kuring-95 [삭제] 사용하지 않는 코드 삭제

--- feature/edit_subscription/src/main/java/com/ku_stacks/ku_ring/edit_subscription/SubscriptionUiModel.kt
+++
@@ -1,8 +0,0 @@
-package com.ku_stacks.ku_ring.edit_subscription
-
-data class SubscriptionUiModel(
-    val content: String,
-    val isNotificationEnabled: Boolean,
-) {
-    fun toggle() = SubscriptionUiModel(content, !isNotificationEnabled)
-}
