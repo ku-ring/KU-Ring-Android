8227f927cf66a4e5552df64d6a0e19d73f6bde75
Wooyoung Myung <mwy3055@gmail.com>
2024-02-05 11:36:41 +0900

    kuring-120 [수정] 더 이상 UiState와 ViewModel에서 선택된 탭 변수를 hold하지 않음

--- feature/edit_subscription/src/main/java/com/ku_stacks/ku_ring/edit_subscription/EditSubscriptionUiState.kt
+++ feature/edit_subscription/src/main/java/com/ku_stacks/ku_ring/edit_subscription/EditSubscriptionUiState.kt
@@ -6,13 +6,11 @@ import com.ku_stacks.ku_ring.edit_subscription.uimodel.NormalSubscriptionUiModel
 data class EditSubscriptionUiState(
     val categories: List<NormalSubscriptionUiModel>,
     val departments: List<DepartmentSubscriptionUiModel>,
-    val selectedTab: EditSubscriptionTab,
 ) {
     companion object {
         val initialValue = EditSubscriptionUiState(
             categories = emptyList(),
             departments = emptyList(),
-            selectedTab = EditSubscriptionTab.NORMAL,
         )
     }
 }
