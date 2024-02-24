2609ac5adef8671a6c3730ad496a8767046dc27a
Wooyoung Myung <mwy3055@gmail.com>
2024-02-24 20:19:40 +0900

    kuring-129 [수정] 변경된 학과 아이템 디자인 적용

--- common/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/components/DepartmentItems.kt
+++ common/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/components/DepartmentItems.kt
@@ -2,6 +2,7 @@ package com.ku_stacks.ku_ring.designsystem.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.contentColorFor
+import androidx.compose.material.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,13 +25,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ku_stacks.ku_ring.designsystem.R
+import com.ku_stacks.ku_ring.designsystem.theme.Gray300
 import com.ku_stacks.ku_ring.designsystem.theme.KuringTheme
+import com.ku_stacks.ku_ring.designsystem.theme.MainPrimarySelected
 import com.ku_stacks.ku_ring.designsystem.theme.Pretendard
+import com.ku_stacks.ku_ring.designsystem.theme.TextCaption1
 import com.ku_stacks.ku_ring.domain.Department
 
 @Composable
@@ -38,7 +45,7 @@ fun DepartmentWithDeleteIcon(
     onDeleteDepartment: (Department) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Department(
+    BaseDepartment(
         department = department,
         modifier = modifier,
     ) {
@@ -46,7 +53,7 @@ fun DepartmentWithDeleteIcon(
             SelectedDepartmentMark()
         }
         Spacer(modifier = Modifier.weight(1f))
-        DeleteIconButton(onClick = { onDeleteDepartment(department) })
+        DeleteButton(onClick = { onDeleteDepartment(department) })
     }
 }
 
@@ -56,7 +63,7 @@ fun DepartmentWithAddIcon(
     onAddDepartment: (Department) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Department(
+    BaseDepartment(
         department = department,
         modifier = modifier,
     ) {
@@ -71,7 +78,7 @@ fun DepartmentWithCheckIcon(
     onClickDepartment: (Department) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Department(
+    BaseDepartment(
         department = department,
         modifier = modifier,
     ) {
@@ -88,15 +95,15 @@ private fun SelectedDepartmentMark(
     Box(
         modifier = modifier
             .clip(shape)
-            .background(MaterialTheme.colors.surface, shape)
+            .background(MainPrimarySelected, shape)
             .border(0.5.dp, MaterialTheme.colors.primary, shape)
-            .padding(horizontal = 8.dp),
+            .padding(horizontal = 8.dp, vertical = 2.dp),
     ) {
         Text(
             text = stringResource(id = R.string.selected_department),
             style = TextStyle(
-                fontSize = 11.sp,
-                lineHeight = 17.93.sp,
+                fontSize = 12.sp,
+                lineHeight = 19.56.sp,
                 fontFamily = Pretendard,
                 fontWeight = FontWeight(600),
                 color = MaterialTheme.colors.primary,
@@ -106,21 +113,25 @@ private fun SelectedDepartmentMark(
 }
 
 @Composable
-private fun DeleteIconButton(
+private fun DeleteButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    contentDescription: String? = null,
 ) {
-    IconButton(
-        onClick = onClick,
-        modifier = modifier,
-    ) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_trashcan),
-            contentDescription = contentDescription,
-            tint = contentColorFor(backgroundColor = MaterialTheme.colors.surface),
-        )
-    }
+    Text(
+        text = stringResource(id = R.string.notice_item_delete_button),
+        style = TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            fontFamily = Pretendard,
+            fontWeight = FontWeight(500),
+            color = TextCaption1,
+            textAlign = TextAlign.End,
+        ),
+        modifier = modifier
+            .clip(RoundedCornerShape(50))
+            .clickable(onClick = onClick, role = Role.Button)
+            .minimumInteractiveComponentSize(),
+    )
 }
 
 @Composable
@@ -136,7 +147,7 @@ private fun AddIconButton(
         Icon(
             painter = painterResource(id = R.drawable.ic_add),
             contentDescription = contentDescription,
-            tint = contentColorFor(backgroundColor = MaterialTheme.colors.surface),
+            tint = Gray300,
         )
     }
 }
@@ -160,7 +171,7 @@ private fun CheckIconButton(
 }
 
 @Composable
-private fun Department(
+private fun BaseDepartment(
     department: Department,
     modifier: Modifier = Modifier,
     contents: @Composable RowScope.() -> Unit = {},
@@ -169,9 +180,9 @@ private fun Department(
     Row(
         modifier = modifier
             .background(backgroundColor)
-            .padding(start = 24.dp, end = 12.dp),
+            .padding(horizontal = 24.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         DepartmentTitle(
             departmentName = department.koreanName,
