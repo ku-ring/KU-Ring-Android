197320f1ea19e7e96a571a924b971a4e9d8405e1
yeon-kyu <cus1128@naver.com>
2024-03-03 23:40:17 +0900

    kuring-99 코드리뷰 반영(StateFlow 대신 바로 value 로 넘기도록 변경)

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/search/compose/inner_screen/StaffSearchScreen.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/search/compose/inner_screen/StaffSearchScreen.kt
@@ -4,17 +4,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.ku_stacks.ku_ring.domain.Staff
-import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun StaffSearchScreen(
-    staffSearchResult: StateFlow<List<Staff>>,
+    staffList: List<Staff>,
     modifier: Modifier = Modifier,
 ) {
-    val staffList = staffSearchResult.collectAsState(initial = emptyList()).value
 
     LazyColumn(modifier) {
         items(staffList) {
