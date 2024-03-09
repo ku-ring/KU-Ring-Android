8e51c0b6c7f22a650c817b4efa9b0ca9bc03c3ff
Wooyoung Myung <mwy3055@gmail.com>
2024-03-09 19:58:42 +0900

    kuring-41 [추가] 온보딩 화면 중 기능 소개 UI 구현

--- feature/onboarding/src/main/java/com/ku_stacks/ku_ring/onboarding/compose/inner_screen/FeatureTabs.kt
+++ feature/onboarding/src/main/java/com/ku_stacks/ku_ring/onboarding/compose/inner_screen/FeatureTabs.kt
@@ -1,27 +1,111 @@
 package com.ku_stacks.ku_ring.onboarding.compose.inner_screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.ku_stacks.ku_ring.designsystem.components.LightAndDarkPreview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ku_stacks.ku_ring.designsystem.components.KuringCallToAction
+import com.ku_stacks.ku_ring.designsystem.components.LightPreview
+import com.ku_stacks.ku_ring.designsystem.components.pager_indicator.HorizontalSlidingIndicator
+import com.ku_stacks.ku_ring.designsystem.theme.Background
 import com.ku_stacks.ku_ring.designsystem.theme.KuringTheme
+import com.ku_stacks.ku_ring.designsystem.theme.Pretendard
+import com.ku_stacks.ku_ring.designsystem.theme.TextTitle
+import com.ku_stacks.ku_ring.onboarding.R
+import com.ku_stacks.ku_ring.onboarding.compose.inner_screen.feature_tab.FeatureTab
+import com.ku_stacks.ku_ring.onboarding.compose.inner_screen.feature_tab.FeatureTabItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun FeatureTabs(
     onNavigateToSetDepartment: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    TextButton(
-        onClick = onNavigateToSetDepartment,
-        modifier = modifier,
+    val tabItems = FeatureTabItem.values()
+    val pagerState = rememberPagerState { tabItems.size }
+    Column(
+        modifier = modifier
+            .background(Background)
+            .fillMaxSize(),
     ) {
-        Text(text = "우리 대학 공지 쿠링이 알려줄게!")
+        FeatureTabsTitle(
+            modifier = Modifier.padding(top = 76.dp, start = 20.dp),
+        )
+        FeatureTabItems(
+            tabItems = tabItems,
+            pagerState = pagerState,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(43.dp))
+        HorizontalSlidingIndicator(
+            pagerState = pagerState,
+            dotSize = 8.dp,
+            spacing = 8.dp,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(vertical = 10.dp),
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        KuringCallToAction(
+            text = stringResource(id = R.string.go_to_department_set),
+            onClick = onNavigateToSetDepartment,
+            enabled = pagerState.currentPage == tabItems.lastIndex,
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
 
-@LightAndDarkPreview
+@Composable
+private fun FeatureTabsTitle(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = stringResource(id = R.string.tab_screen_title),
+        modifier = modifier,
+        style = TextStyle(
+            fontSize = 24.sp,
+            lineHeight = 34.08.sp,
+            fontFamily = Pretendard,
+            fontWeight = FontWeight(700),
+            color = TextTitle,
+        )
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun FeatureTabItems(
+    tabItems: Array<FeatureTabItem>,
+    pagerState: PagerState,
+    modifier: Modifier = Modifier,
+) {
+    HorizontalPager(
+        state = pagerState,
+        modifier = modifier,
+    ) {
+        FeatureTab(
+            item = tabItems[it],
+        )
+    }
+}
+
+@LightPreview
 @Composable
 private fun FeatureTabsPreview() {
     KuringTheme {
