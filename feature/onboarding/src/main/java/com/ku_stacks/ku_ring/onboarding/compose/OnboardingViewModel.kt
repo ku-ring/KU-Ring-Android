e46d03cc188e3ccdeaa90dcf3f0ea65129ce00a8
Wooyoung Myung <mwy3055@gmail.com>
2024-03-10 20:49:30 +0900

    kuring-41 [추가] 학과 검색 관련 비즈니스 로직 추가

---
+++ feature/onboarding/src/main/java/com/ku_stacks/ku_ring/onboarding/compose/OnboardingViewModel.kt
@@ -0,0 +1,47 @@
+package com.ku_stacks.ku_ring.onboarding.compose
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ku_stacks.ku_ring.department.repository.DepartmentRepository
+import com.ku_stacks.ku_ring.domain.Department
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(
+    private val departmentRepository: DepartmentRepository,
+) : ViewModel() {
+    private val _query = MutableStateFlow<String?>(null)
+    val query: StateFlow<String?>
+        get() = _query
+
+    private val _departments = MutableStateFlow<List<Department>>(emptyList())
+    val departments: StateFlow<List<Department>>
+        get() = _departments
+
+    private val _isInitialSearch = MutableStateFlow(true)
+    val isInitialSearch: StateFlow<Boolean>
+        get() = _isInitialSearch
+
+    init {
+        viewModelScope.launch {
+            departmentRepository.updateDepartmentsFromRemote()
+        }
+    }
+
+    fun onQueryUpdate(newQuery: String) {
+        _query.value = newQuery
+    }
+
+    fun search() {
+        _isInitialSearch.value = false
+        viewModelScope.launch {
+            _departments.value =
+                departmentRepository.getDepartmentsByKoreanName(query.value ?: return@launch)
+            departmentRepository.getSubscribedDepartments()
+        }
+    }
+}
