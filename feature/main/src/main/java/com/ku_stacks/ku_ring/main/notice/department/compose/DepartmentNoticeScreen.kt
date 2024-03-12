ee3bd7ac61b2cec022118d98f7b9d9cf5f2bfcf0
Wooyoung Myung <mwy3055@gmail.com>
2024-03-12 17:35:51 +0900

    kuring-96 [추가] 학과별 공지 composable의 오버로딩 추가

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/department/compose/DepartmentNoticeScreen.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/department/compose/DepartmentNoticeScreen.kt
@@ -17,9 +17,15 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -33,16 +39,52 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.ku_stacks.ku_ring.designsystem.components.LazyPagingNoticeItemColumn
 import com.ku_stacks.ku_ring.domain.Department
 import com.ku_stacks.ku_ring.domain.Notice
 import com.ku_stacks.ku_ring.main.R
+import com.ku_stacks.ku_ring.main.notice.department.DepartmentNoticeViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun DepartmentNoticeScreen(
+internal fun DepartmentNoticeScreen(
+    viewModel: DepartmentNoticeViewModel,
+    onNoticeClick: (Notice) -> Unit,
+    onShowDepartmentSubscribeBottomSheet: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val selectedDepartments by viewModel.subscribedDepartments.collectAsState()
+    val noticesFlow by viewModel.currentDepartmentNotice.collectAsState()
+    val notices = noticesFlow?.collectAsLazyPagingItems()
+
+    var isRefreshing by remember { mutableStateOf(false) }
+    val refreshState = rememberPullRefreshState(
+        refreshing = isRefreshing,
+        onRefresh = {
+            isRefreshing = true
+            notices?.refresh()
+            isRefreshing = false
+        },
+        refreshThreshold = 100.dp,
+    )
+    DepartmentNoticeScreen(
+        selectedDepartments = selectedDepartments,
+        onSelectDepartment = viewModel::selectDepartment,
+        notices = notices,
+        onNoticeClick = onNoticeClick,
+        onFabClick = onShowDepartmentSubscribeBottomSheet,
+        isRefreshing = isRefreshing,
+        refreshState = refreshState,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun DepartmentNoticeScreen(
     selectedDepartments: List<Department>,
     onSelectDepartment: (Department) -> Unit,
     notices: LazyPagingItems<Notice>?,
