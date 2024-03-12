8c861cb150d8acae5f7083d75b9d9e8463868897
Wooyoung Myung <mwy3055@gmail.com>
2024-03-12 14:54:41 +0900

    kuring-41 [수정] 알림을 보내는 코드를 :common:util로 옮김

--- common/thirdparty/src/main/java/com/ku_stacks/ku_ring/thirdparty/firebase/MyFireBaseMessagingService.kt
+++ common/thirdparty/src/main/java/com/ku_stacks/ku_ring/thirdparty/firebase/MyFireBaseMessagingService.kt
@@ -10,9 +10,9 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.ku_stacks.ku_ring.local.room.PushDao
 import com.ku_stacks.ku_ring.preferences.PreferenceUtil
-import com.ku_stacks.ku_ring.thirdparty.R
 import com.ku_stacks.ku_ring.ui_util.KuringNavigator
 import com.ku_stacks.ku_ring.util.DateUtil
+import com.ku_stacks.ku_ring.util.KuringNotificationManager
 import com.ku_stacks.ku_ring.util.WordConverter
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -89,60 +89,12 @@ class MyFireBaseMessagingService : FirebaseMessagingService() {
         articleId: String?,
         category: String?
     ) {
-        val intent = navigator.createNoticeWebIntent(
-            this,
-            url,
-            articleId,
-            category,
-        )
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val channelId = CHANNEL_ID
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notification))
-            .setSmallIcon(R.drawable.ic_status_bar)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setSound(defaultSound)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
+        val intent = navigator.createNoticeWebIntent(this, url, articleId, category)
+        KuringNotificationManager.showNotificationWithUrl(this, intent, title, body)
     }
 
     private fun showCustomNotification(type: String, title: String, body: String) {
         val intent = navigator.createMainIntent(this)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val channelId = CHANNEL_ID
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notification))
-            .setSmallIcon(R.drawable.ic_status_bar)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setSound(defaultSound)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(1, notificationBuilder.build())
-    }
-
-    companion object {
-        const val CHANNEL_ID = "ku_stack_channel_id"
-        const val CHANNEL_NAME = "쿠링"
+        KuringNotificationManager.showCustomNotification(this, intent, type, title, body)
     }
 }
