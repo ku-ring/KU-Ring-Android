ddef2eea7c543be6a1cf3247580c3e01bbe157f6
Wooyoung Myung <mwy3055@gmail.com>
2024-03-12 17:55:35 +0900

    kuring-96 [수정] 학과별 공지 헤더를 2.0 디자인에 맞게 수정

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/department/compose/DepartmentHeader.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/department/compose/DepartmentHeader.kt
@@ -1,80 +1,68 @@
 package com.ku_stacks.ku_ring.main.notice.department.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ku_stacks.ku_ring.designsystem.components.LightAndDarkPreview
 import com.ku_stacks.ku_ring.designsystem.theme.KuringTheme
-import com.ku_stacks.ku_ring.designsystem.theme.SfProDisplay
+import com.ku_stacks.ku_ring.designsystem.theme.Pretendard
+import com.ku_stacks.ku_ring.designsystem.theme.TextBody
 import com.ku_stacks.ku_ring.main.R
 
 @Composable
 fun DepartmentHeader(
     selectedDepartmentName: String,
     onClick: () -> Unit,
-    showArrow: Boolean,
     modifier: Modifier = Modifier,
-    isClickable: Boolean = true,
 ) {
-    val description =
-        if (showArrow) stringResource(id = R.string.department_selector_description) else ""
+    val description = stringResource(id = R.string.department_selector_description)
 
-    Button(
-        onClick = {
-            if (isClickable) {
-                onClick()
-            }
-        },
+    Row(
         modifier = modifier
-            .clip(RoundedCornerShape(15.dp))
             .clearAndSetSemantics {
                 contentDescription = description
-            },
-        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.kus_tertiary_label)),
-        elevation = null,
+                role = Role.Button
+            }
+            .clickable(onClick = onClick)
+            .padding(horizontal = 20.dp, vertical = 21.5.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
             text = selectedDepartmentName,
-            fontFamily = SfProDisplay,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-            color = colorResource(id = R.color.kus_on_tertiary_label),
-            modifier = Modifier.padding(vertical = 5.dp),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 27.sp,
+                fontFamily = Pretendard,
+                fontWeight = FontWeight(700),
+                color = TextBody,
+            ),
         )
         Spacer(modifier = Modifier.weight(1f))
-        if (showArrow) {
-            Image(
-                painter = painterResource(R.drawable.ic_arrow),
-                contentDescription = null,
-                modifier = Modifier
-                    .rotate(90f)
-                    .scale(0.8f),
-                colorFilter = ColorFilter.tint(colorResource(R.color.kus_on_tertiary_label)),
-            )
-        }
+        Image(
+            painter = painterResource(R.drawable.ic_chevron_2),
+            contentDescription = null,
+        )
     }
 }
 
@@ -86,10 +74,7 @@ private fun DepartmentHeaderPreview() {
         DepartmentHeader(
             selectedDepartmentName = "산업디자인학과",
             onClick = { showArrow = !showArrow },
-            showArrow = showArrow,
-            modifier = Modifier
-                .padding(10.dp)
-                .width(320.dp),
+            modifier = Modifier.fillMaxWidth(),
         )
     }
 }
