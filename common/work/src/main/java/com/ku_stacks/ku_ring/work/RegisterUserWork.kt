b016f7ae6494efe05031320166022897659415f9
Wooyoung Myung <mwy3055@gmail.com>
2024-03-12 14:55:14 +0900

    kuring-41 [추가] v2 사용자 등록 API를 실행하는 worker 구현

---
+++ common/work/src/main/java/com/ku_stacks/ku_ring/work/RegisterUserWork.kt
@@ -0,0 +1,39 @@
+package com.ku_stacks.ku_ring.work
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.Data
+import androidx.work.WorkerParameters
+import com.ku_stacks.ku_ring.user.repository.UserRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import timber.log.Timber
+
+@HiltWorker
+class RegisterUserWork @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val userRepository: UserRepository,
+) : CoroutineWorker(appContext, workerParams) {
+    override suspend fun doWork(): Result {
+        val token = getToken() ?: return Result.failure()
+        userRepository.registerUser(token)
+        Timber.d("registered token successfully")
+        return Result.success()
+    }
+
+    private fun getToken(): String? {
+        return inputData.getString(TOKEN_KEY)
+    }
+
+    companion object {
+        fun createData(token: String): Data {
+            return Data.Builder()
+                .putString(TOKEN_KEY, token)
+                .build()
+        }
+
+        private const val TOKEN_KEY = "token-key"
+    }
+}
