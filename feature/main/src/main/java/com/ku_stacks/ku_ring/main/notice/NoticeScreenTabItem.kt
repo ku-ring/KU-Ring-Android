f2ea480b707feaf4b101a511b1a01e01e5ba8278
Wooyoung Myung <mwy3055@gmail.com>
2024-03-13 19:37:14 +0900

    kuring-96 [추가] 카테고리의 순서를 저장하는 enum class 추가

---
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/NoticeScreenTabItem.kt
@@ -0,0 +1,13 @@
+package com.ku_stacks.ku_ring.main.notice
+
+enum class NoticeScreenTabItem(val shortCategoryName: String, val koreanName: String) {
+    DEPARTMENT("dep", "학과"),
+    BACHELOR("bch", "학사"),
+    EMPLOYMENT("emp", "취창업"),
+    INTERNATIONAL("nat", "국제"),
+    SCHOLARSHIP("sch", "장학"),
+    LIBRARY("lib", "도서관"),
+    STUDENT("stu", "학생"),
+    INDUSTRY_UNIVERSITY("ind", "산학"),
+    NORMAL("nor", "일반");
+}
