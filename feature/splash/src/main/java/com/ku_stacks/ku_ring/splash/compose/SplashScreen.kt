21b62ebd520f1c47d580b2ef65d9eb35903be71b
Wooyoung Myung <mwy3055@gmail.com>
2024-03-14 14:10:20 +0900

    kuring-117 [추가] v2 스플래시 UI 구현

---
+++ feature/splash/src/main/java/com/ku_stacks/ku_ring/splash/compose/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.ku_stacks.ku_ring.splash.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.ku_stacks.ku_ring.designsystem.components.LightAndDarkPreview
+import com.ku_stacks.ku_ring.designsystem.theme.KuringTheme
+import com.ku_stacks.ku_ring.splash.R
+
+@Composable
+internal fun SplashScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_app),
+            contentDescription = stringResource(id = R.string.splash_app_icon),
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}
+
+@LightAndDarkPreview
+@Composable
+private fun SplashScreenPreview() {
+    KuringTheme {
+        SplashScreen(modifier = Modifier.fillMaxSize())
+    }
+}
