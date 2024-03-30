5d5545492426916732b04b41b8847e199d1b72fc
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-03-30 19:24:41 +0900

    Add Keyboard Utils

---
+++ common/ui_util/src/main/java/com/ku_stacks/ku_ring/ui_util/Keyboard.kt
@@ -0,0 +1,32 @@
+package com.ku_stacks.ku_ring.ui_util
+
+import android.view.ViewTreeObserver
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Composable
+fun rememberKeyboardOpen(): State<Boolean> {
+    val keyboardState = remember { mutableStateOf(false) }
+    val view = LocalView.current
+
+    DisposableEffect(view) {
+        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
+            val isKeyboardOpen =
+                ViewCompat.getRootWindowInsets(view)?.isVisible(WindowInsetsCompat.Type.ime())
+                    ?: false
+            keyboardState.value = isKeyboardOpen
+        }
+        view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
+        }
+    }
+    return keyboardState
+}
