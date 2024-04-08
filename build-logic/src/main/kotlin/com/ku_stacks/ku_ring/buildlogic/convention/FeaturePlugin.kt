ad38b6c98e88b2ff93a57fd98c8017137c13881b
Lee HyunWoo <l2hyunwoo@gmail.com>
2024-04-08 16:41:54 +0900

    Separate feature plugin from convention plugins

--- build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/FeaturePlugin.kt
+++ build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/FeaturePlugin.kt
@@ -2,18 +2,21 @@ package com.ku_stacks.ku_ring.buildlogic.convention
 
 import com.ku_stacks.ku_ring.buildlogic.dsl.configureAndroidLibrary
 import com.ku_stacks.ku_ring.buildlogic.primitive.CommonAndroidPlugin
-import com.ku_stacks.ku_ring.buildlogic.primitive.ComposePlugin
-import com.ku_stacks.ku_ring.buildlogic.primitive.FirebasePlugin
 import com.ku_stacks.ku_ring.buildlogic.primitive.HiltPlugin
 import com.ku_stacks.ku_ring.buildlogic.primitive.KotlinPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 
-class ComposePlugin : Plugin<Project> {
+class FeaturePlugin: Plugin<Project> {
     override fun apply(target: Project) = with(target) {
-        apply<FeaturePlugin>()
-        apply<ComposePlugin>()
+        with(pluginManager) {
+            apply("com.android.library")
+        }
+
+        apply<KotlinPlugin>()
+        apply<CommonAndroidPlugin>()
+        apply<HiltPlugin>()
         configureAndroidLibrary()
     }
 }
--- build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/ViewBasedFeaturePlugin.kt
+++ build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/ViewBasedFeaturePlugin.kt
@@ -1,14 +1,10 @@
 package com.ku_stacks.ku_ring.buildlogic.convention
 
 import com.android.build.gradle.BaseExtension
-import com.ku_stacks.ku_ring.buildlogic.dsl.configureAndroidLibrary
 import com.ku_stacks.ku_ring.buildlogic.dsl.debugImplementation
 import com.ku_stacks.ku_ring.buildlogic.dsl.implementation
 import com.ku_stacks.ku_ring.buildlogic.dsl.library
 import com.ku_stacks.ku_ring.buildlogic.dsl.libs
-import com.ku_stacks.ku_ring.buildlogic.primitive.CommonAndroidPlugin
-import com.ku_stacks.ku_ring.buildlogic.primitive.HiltPlugin
-import com.ku_stacks.ku_ring.buildlogic.primitive.KotlinPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
@@ -17,14 +13,7 @@ import org.gradle.kotlin.dsl.getByType
 
 class ViewBasedFeaturePlugin: Plugin<Project> {
     override fun apply(target: Project) = with(target) {
-        with(pluginManager) {
-            apply("com.android.library")
-        }
-
-        apply<KotlinPlugin>()
-        apply<CommonAndroidPlugin>()
-        apply<HiltPlugin>()
-        configureAndroidLibrary()
+        apply<FeaturePlugin>()
 
         extensions.getByType<BaseExtension>().apply {
             buildFeatures.apply {
