9e65a3ddb0853bb4537b539c56541b7a6baffdfb
Lee HyunWoo <l2hyunwoo@gmail.com>
2024-04-08 16:31:47 +0900

    Update common designsystem module build script

---
+++ common/designsystem/build.gradle.kts
@@ -0,0 +1,27 @@
+import com.ku_stacks.ku_ring.buildlogic.dsl.setNameSpace
+
+plugins {
+    kuring("compose")
+}
+
+android {
+    setNameSpace("designsystem")
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.data.domain)
+}
