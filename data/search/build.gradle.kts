b71bc7d78283c778d4bb60297f793eebcdb4d52e
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-09 21:41:51 +0900

    [feature/apply-build-script-on-data-search] Apply kts

---
+++ data/search/build.gradle.kts
@@ -0,0 +1,27 @@
+import com.ku_stacks.ku_ring.buildlogic.dsl.setNameSpace
+
+plugins {
+    kuring("feature")
+    kuringPrimitive("room")
+    kuringPrimitive("test")
+    kuringPrimitive("retrofit")
+}
+
+android {
+    setNameSpace("search")
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.common.util)
+    implementation(projects.data.domain)
+    implementation(projects.data.local)
+    testImplementation(testFixtures(projects.data.local))
+    implementation(projects.data.remote)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+}
