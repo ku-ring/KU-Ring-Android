c1a8e9f242faf529765bf179cec801868df6e017
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2024-06-01 17:17:13 +0900

    :wrench: Convert groovy based to kotlin based in data-space module (#223)

---
+++ data/space/build.gradle.kts
@@ -0,0 +1,23 @@
+import com.ku_stacks.ku_ring.buildlogic.dsl.setNameSpace
+
+plugins {
+    kuring("feature")
+    kuringPrimitive("retrofit")
+    kuringPrimitive("test")
+}
+
+android {
+    setNameSpace("space")
+}
+
+dependencies {
+    implementation(projects.common.util)
+    implementation(projects.data.remote)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    // okHttp
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    testImplementation(libs.okhttp.mockwebserver)
+}
