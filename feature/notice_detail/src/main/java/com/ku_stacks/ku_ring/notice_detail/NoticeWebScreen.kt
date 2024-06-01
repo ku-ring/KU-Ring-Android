055996cad9c516e9a4a02df39855f7c902ea30f3
Wooyoung Myung <mwy3055@gmail.com>
2024-06-01 14:51:59 +0900

    kuring-167 [추가] 공지 웹뷰 페이지를 compose로 migrate

---
+++ feature/notice_detail/src/main/java/com/ku_stacks/ku_ring/notice_detail/NoticeWebScreen.kt
@@ -0,0 +1,146 @@
+package com.ku_stacks.ku_ring.notice_detail
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ShareCompat
+import com.ku_stacks.ku_ring.designsystem.components.KuringWebView
+import com.ku_stacks.ku_ring.designsystem.components.LightAndDarkPreview
+import com.ku_stacks.ku_ring.designsystem.components.topbar.CenterTitleTopBar
+import com.ku_stacks.ku_ring.designsystem.kuringtheme.KuringTheme
+import com.ku_stacks.ku_ring.domain.WebViewNotice
+import com.ku_stacks.ku_ring.util.WordConverter
+
+@Composable
+fun NoticeWebScreen(
+    webViewNotice: WebViewNotice,
+    isSaved: Boolean,
+    onNavigateBack: () -> Unit,
+    onSaveButtonClick: () -> Unit,
+    afterPageLoaded: (WebViewNotice) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    DisposableEffect(webViewNotice) {
+        onDispose {
+            afterPageLoaded(webViewNotice)
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            CenterTitleTopBar(
+                title = WordConverter.convertEnglishToKorean(webViewNotice.category),
+                modifier = Modifier.fillMaxWidth(),
+                navigation = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_back_v2),
+                        contentDescription = stringResource(id = R.string.navigation_button_description),
+                    )
+                },
+                onNavigationClick = onNavigateBack,
+                action = {
+                    NoticeWebScreenActions(
+                        isSaved = isSaved,
+                        onSaveButtonClick = onSaveButtonClick,
+                        webViewNotice = webViewNotice,
+                    )
+                }
+            )
+        },
+        modifier = modifier,
+    ) {
+        KuringWebView(
+            url = webViewNotice.url,
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            customSettings = {
+                builtInZoomControls = true
+            },
+        )
+    }
+}
+
+@Composable
+private fun NoticeWebScreenActions(
+    isSaved: Boolean,
+    onSaveButtonClick: () -> Unit,
+    webViewNotice: WebViewNotice,
+    modifier: Modifier = Modifier,
+) {
+    val bookmarkIconId =
+        if (isSaved) R.drawable.ic_bookmark_fill_v2 else R.drawable.ic_bookmark_v2
+    val context = LocalContext.current
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = bookmarkIconId),
+            contentDescription = stringResource(id = R.string.save_button_description),
+            tint = KuringTheme.colors.gray600,
+            modifier = Modifier
+                .clickable(role = Role.Switch, onClick = onSaveButtonClick),
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_share_v2),
+            contentDescription = stringResource(id = R.string.share_externally_description),
+            tint = KuringTheme.colors.gray600,
+            modifier = Modifier.clickable(
+                role = Role.Button,
+                onClick = { onShareButtonClick(context, webViewNotice.url) }
+            ),
+        )
+    }
+}
+
+private fun onShareButtonClick(context: Context, url: String) {
+    ShareCompat.IntentBuilder(context)
+        .setChooserTitle(R.string.share_externally_description)
+        .setText(url)
+        .setType("text/plain")
+        .startChooser()
+}
+
+@LightAndDarkPreview
+@Composable
+private fun NoticeWebScreenPreview() {
+    var isSaved by remember { mutableStateOf(false) }
+    KuringTheme {
+        NoticeWebScreen(
+            webViewNotice = WebViewNotice(
+                url = "https://www.google.com",
+                articleId = "123",
+                category = "학사",
+                subject = "쿠링 발전의 건에 대하여",
+            ),
+            isSaved = isSaved,
+            onNavigateBack = {},
+            onSaveButtonClick = { isSaved = !isSaved },
+            afterPageLoaded = {},
+            modifier = Modifier
+                .background(KuringTheme.colors.background)
+                .fillMaxSize(),
+        )
+    }
+}
