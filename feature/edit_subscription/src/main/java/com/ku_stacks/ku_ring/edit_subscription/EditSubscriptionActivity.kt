5bb0775a94999a11bf8821bfffdcc58a398208d5
Wooyoung Myung <mwy3055@gmail.com>
2024-06-02 14:54:32 +0900

    kuring-169 [수정] 구독 수정 화면의 view를 제거하고, 로직을 모두 compose로 옮김

--- feature/edit_subscription/src/main/java/com/ku_stacks/ku_ring/edit_subscription/EditSubscriptionActivity.kt
+++ feature/edit_subscription/src/main/java/com/ku_stacks/ku_ring/edit_subscription/EditSubscriptionActivity.kt
@@ -3,14 +3,12 @@ package com.ku_stacks.ku_ring.edit_subscription
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.databinding.DataBindingUtil
 import com.ku_stacks.ku_ring.designsystem.kuringtheme.KuringTheme
 import com.ku_stacks.ku_ring.edit_subscription.compose.EditSubscriptionScreen
-import com.ku_stacks.ku_ring.edit_subscription.databinding.ActivityEditSubscriptionBinding
 import com.ku_stacks.ku_ring.thirdparty.compose.KuringCompositionLocalProvider
 import com.ku_stacks.ku_ring.thirdparty.di.LocalNavigator
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,44 +16,26 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class EditSubscriptionActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityEditSubscriptionBinding
-    private val viewModel by viewModels<EditSubscriptionViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupBinding()
-        setupView()
-    }
-
-    private fun setupBinding() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_subscription)
-        binding.lifecycleOwner = this
-    }
-
-    private fun setupView() {
-        viewModel.firstRunFlag = intent.getBooleanExtra(FIRST_RUN_FLAG, false)
-        binding.composeView.setContent {
+        setContent {
             KuringCompositionLocalProvider {
                 val navigator = LocalNavigator.current
                 KuringTheme {
                     EditSubscriptionScreen(
-                        viewModel = viewModel,
                         onNavigateToBack = ::finish,
-                        onAddDepartmentButtonClick = { navigator.navigateToEditSubscribedDepartment(this) },
-                        onSubscriptionComplete = ::onSubscriptionComplete,
+                        onAddDepartmentButtonClick = {
+                            navigator.navigateToEditSubscribedDepartment(this)
+                        },
+                        onFinish = {
+                            setResult(RESULT_OK)
+                            finish()
+                        },
                         modifier = Modifier.fillMaxSize(),
                     )
                 }
             }
-        }
-    }
-
-    private fun onSubscriptionComplete() {
-        if (viewModel.isInitialLoadDone) {
-            viewModel.saveSubscribe()
-            setResult(RESULT_OK)
-            finish()
         }
     }
 
