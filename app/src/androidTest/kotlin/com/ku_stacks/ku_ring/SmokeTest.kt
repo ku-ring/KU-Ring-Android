a51d0f12e4e5370d06c723c0a76c7ff66eeabc95
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-06-02 18:32:47 +0900

    [feature/kuring-163] SmokeTest 수정

--- app/src/androidTest/kotlin/com/ku_stacks/ku_ring/SmokeTest.kt
+++ app/src/androidTest/kotlin/com/ku_stacks/ku_ring/SmokeTest.kt
@@ -1,42 +1,23 @@
 package com.ku_stacks.ku_ring
 
-import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ku_stacks.ku_ring.splash.SplashActivity
-import com.ku_stacks.ku_ring.splash.SplashScreenState
-import com.ku_stacks.ku_ring.splash.compose.SplashScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SmokeTest {
+
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
-    private lateinit var navController: TestNavHostController
+    val composeTestRule = createAndroidComposeRule<SplashActivity>()
 
     @Test
     fun testAppLaunch() {
-        Intents.init()
-        composeTestRule.setContent {
-            val context = LocalContext.current
-            navController = TestNavHostController(context)
-            navController.navigatorProvider.addNavigator(ComposeNavigator())
-            SplashScreen(
-                screenState = SplashScreenState.LOADING,
-                onUpdateApp = {},
-                onDismissUpdateDialog = {},
-                modifier = androidx.compose.ui.Modifier.fillMaxSize()
-            )
-        }
-        Intents.intended(hasComponent(SplashActivity::class.java.name))
-        Intents.release()
+        composeTestRule
+            .onNodeWithContentDescription("스플래시 화면")
+            .assertExists()
     }
 }
