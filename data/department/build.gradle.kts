e79ea2dee7081af4b1a585b7c51ede8a56d1c77b
Wooyoung Myung <mwy3055@gmail.com>
2024-06-15 18:43:21 +0900

    [수정] 이제 :data:department가 :test 모듈의 로직을 사용

--- data/department/build.gradle.kts
+++ data/department/build.gradle.kts
@@ -24,6 +24,6 @@ dependencies {
     implementation(projects.data.local)
     implementation(projects.data.remote)
 
-    testImplementation(projects.data.domain.testUtils)
+    testImplementation(projects.data.department.test)
     testImplementation(libs.kotlinx.coroutines.test)
 }
