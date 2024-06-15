e43ec99b31f8d20d715daab8c736a5b7e67b5628
Wooyoung Myung <mwy3055@gmail.com>
2024-06-15 21:16:48 +0900

    [수정] 불필요한 Class<T> 매개변수 제거

--- common/testUtil/src/main/java/com/ku_stacks/ku_ring/testutil/MockUtil.kt
+++ common/testUtil/src/main/java/com/ku_stacks/ku_ring/testutil/MockUtil.kt
@@ -3,5 +3,5 @@ package com.ku_stacks.ku_ring.testutil
 import org.mockito.Mockito
 
 object MockUtil {
-    inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
+    inline fun <reified T> mock(): T = Mockito.mock()
 }
