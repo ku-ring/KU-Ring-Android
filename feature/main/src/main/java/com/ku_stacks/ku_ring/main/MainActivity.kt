c09f2cfa0dd9c451113f9ffc9bece15f6664bb9b
Wooyoung Myung <mwy3055@gmail.com>
2024-06-18 19:28:42 +0900

    [수정] MainActivity에서 XML 대신 Compose MainScreen을 호출

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/MainActivity.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/MainActivity.kt
@@ -4,11 +4,18 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.ku_stacks.ku_ring.designsystem.kuringtheme.KuringTheme
 import com.ku_stacks.ku_ring.domain.WebViewNotice
-import com.ku_stacks.ku_ring.main.databinding.ActivityMainBinding
 import com.ku_stacks.ku_ring.ui_util.KuringNavigator
 import com.ku_stacks.ku_ring.ui_util.showToast
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,22 +24,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     @Inject
     lateinit var navigator: KuringNavigator
-
-    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
-        override fun onPageSelected(position: Int) {
-            binding.mainBottomNavigation.selectedItemId = when (position) {
-                0 -> R.id.notice_screen
-                1 -> R.id.archive_screen
-                2 -> R.id.campus_map_screen
-                3 -> R.id.setting_screen
-                else -> throw IllegalStateException("no such main viewpager position")
-            }
-        }
-    }
 
     private var backPressedTime = 0L
 
@@ -51,40 +44,23 @@ class MainActivity : AppCompatActivity() {
             navToNoticeActivity(webViewNotice)
         }
 
-        setupBinding()
-        setupView()
-    }
-
-    private fun setupBinding() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    private fun setupView() {
-        val pagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
-
-        binding.mainViewPager.apply {
-            adapter = pagerAdapter
-            registerOnPageChangeCallback(pageChangeCallback)
-            isUserInputEnabled = false
-            offscreenPageLimit = pagerAdapter.itemCount
+        setContent {
+            KuringTheme {
+                var currentRoute: MainScreenRoute by remember { mutableStateOf(MainScreenRoute.Notice) }
+                val navController = rememberNavController()
+                MainScreen(
+                    navController = navController,
+                    currentRoute = currentRoute,
+                    onNavigateToRoute = {
+                        if (currentRoute != it) {
+                            currentRoute = it
+                            navController.navigate(it)
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize().background(KuringTheme.colors.background),
+                )
+            }
         }
-
-        binding.mainBottomNavigation.setOnItemSelectedListener {
-            navigationSelected(it)
-        }
-    }
-
-    private fun navigationSelected(item: MenuItem): Boolean {
-        when (item.setChecked(true).itemId) {
-            R.id.notice_screen -> binding.mainViewPager.setCurrentItem(0, false)
-            R.id.archive_screen -> binding.mainViewPager.setCurrentItem(1, false)
-            R.id.campus_map_screen -> binding.mainViewPager.setCurrentItem(2, false)
-            R.id.setting_screen -> binding.mainViewPager.setCurrentItem(3, false)
-            else -> return false
-        }
-
-        return true
     }
 
     private fun navToNoticeActivity(webViewNotice: WebViewNotice) {
@@ -98,11 +74,6 @@ class MainActivity : AppCompatActivity() {
             showToast(getString(R.string.home_finish_if_back_again))
             backPressedTime = System.currentTimeMillis()
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding.mainViewPager.unregisterOnPageChangeCallback(pageChangeCallback)
     }
 
     companion object {
