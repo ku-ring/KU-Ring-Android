73c69d78e28abaf92afae0c61bd16b2e7f59e23a
Wooyoung Myung <mwy3055@gmail.com>
2024-06-18 14:50:12 +0900

    [수정] 메인 화면 route에 @Serializable 어노테이션 추가

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/MainScreenRoute.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/MainScreenRoute.kt
@@ -1,9 +1,18 @@
 package com.ku_stacks.ku_ring.main
 
+import kotlinx.serialization.Serializable
+
 sealed interface MainScreenRoute {
+    @Serializable
     data object Notice : MainScreenRoute
+
+    @Serializable
     data object Archive : MainScreenRoute
+
+    @Serializable
     data object CampusMap : MainScreenRoute
+
+    @Serializable
     data object Settings : MainScreenRoute
 
     companion object {
