c1b50bfd6e2a42c4aa2ff3b802cfbca54e16b2c9
Wooyoung Myung <mwy3055@gmail.com>
2024-07-24 21:14:33 +0900

    test: SSERemoteDataSource 테스트 코드 추가

---
+++ data/remote/src/test/java/com/ku_stacks/ku_ring/remote/SSERemoteDataSourceTest.kt
@@ -0,0 +1,41 @@
+package com.ku_stacks.ku_ring.remote
+
+import com.ku_stacks.ku_ring.remote.sse.SSEDataSource
+import com.ku_stacks.ku_ring.remote.sse.SSERemoteDataSource
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.sse.SSE
+import junit.framework.TestCase.assertNotNull
+import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class SSERemoteDataSourceTest {
+    private val sseDataSource: SSEDataSource = SSERemoteDataSource(
+        HttpClient(CIO) {
+            install(SSE) {
+                showCommentEvents()
+                showRetryEvents()
+            }
+        }
+    )
+
+    private val query = "교내,외 장학금 및 학자금 대출 관련 전화번호들을 안내를 해줘"
+
+    @Test
+    fun `test session is opened successfully`() = runTest {
+        // given
+        val testToken = System.currentTimeMillis().toString()
+
+        // when
+        val session = sseDataSource.openKuringBotSSESession(query, testToken)
+        val response = session?.reduce { accumulator, value -> accumulator + value }
+        println("response: $response")
+
+        // then
+        assertNotNull(session)
+        assert(response?.isNotEmpty() ?: false)
+    }
+
+
+}
