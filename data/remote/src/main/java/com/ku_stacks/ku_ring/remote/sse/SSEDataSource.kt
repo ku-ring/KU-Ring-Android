5332bec5fd84ca893c58a17c8b8675923c660f58
Wooyoung Myung <mwy3055@gmail.com>
2024-07-25 22:22:06 +0900

    fix: SSEDataSource가 람다식을 받도록 수정

--- data/remote/src/main/java/com/ku_stacks/ku_ring/remote/sse/SSEDataSource.kt
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/sse/SSEDataSource.kt
@@ -1,18 +1,17 @@
 package com.ku_stacks.ku_ring.remote.sse
 
-import kotlinx.coroutines.flow.Flow
-
 interface SSEDataSource {
     /**
-     * 쿠링봇 SSE 세션을 열고, [Flow] 포맷으로 반환한다.
+     * 쿠링봇 SSE 세션을 열고, 데이터를 문자열 형식으로 받아 처리한다.
+     * SSE 데이터를 받았을 때, [onData]에 접두어 `data:`를 떼지 않고 그대로 제공한다.
      *
      * @param query 질문 내용
      * @param token 사용자 FCM 토큰
-     *
-     * @return 세션을 정상적으로 연 경우 non-null [Flow], 오류가 발생한 경우 null
+     * @param onData 데이터를 수신했을 때 실행할 작업.
      */
     suspend fun openKuringBotSSESession(
         query: String,
         token: String,
-    ): Flow<String>?
+        onData: (String) -> Unit,
+    )
 }
