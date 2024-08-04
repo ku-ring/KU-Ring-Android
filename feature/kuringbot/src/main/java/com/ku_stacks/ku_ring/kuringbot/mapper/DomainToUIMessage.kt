60d39f7b75146bac8a8a5fa05b35e8b582b65fe5
Wooyoung Myung <mwy3055@gmail.com>
2024-08-04 22:14:45 +0900

    feature: 쿠링봇 화면에서 보여주는 메시지 종류 선언

---
+++ feature/kuringbot/src/main/java/com/ku_stacks/ku_ring/kuringbot/mapper/DomainToUIMessage.kt
@@ -0,0 +1,18 @@
+package com.ku_stacks.ku_ring.kuringbot.mapper
+
+import com.ku_stacks.ku_ring.domain.KuringBotMessage
+import com.ku_stacks.ku_ring.kuringbot.KuringBotUIMessage
+
+internal fun KuringBotMessage.toUIMessage(): KuringBotUIMessage = when (isQuery) {
+    true -> KuringBotUIMessage.Question(
+        id = id,
+        message = message,
+        postedTime = postedDate,
+    )
+
+    false -> KuringBotUIMessage.Response(
+        id = id,
+        message = message,
+        postedTime = postedDate,
+    )
+}
