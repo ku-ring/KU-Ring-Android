4d08c49e9cdd8f524bec7c4ca2307c6dd9b30f05
Wooyoung Myung <mwy3055@gmail.com>
2024-08-23 16:45:37 +0900

    feature: 지원하지 않는 학과 UI 구현

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/compose/inner_screen/DepartmentNoticeScreen.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/compose/inner_screen/DepartmentNoticeScreen.kt
@@ -2,14 +2,34 @@ package com.ku_stacks.ku_ring.main.notice.compose.inner_screen
 
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.PullRefreshState
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -22,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.ku_stacks.ku_ring.department.UnsupportedDepartmentUtil
 import com.ku_stacks.ku_ring.designsystem.components.KuringCallToAction
 import com.ku_stacks.ku_ring.designsystem.components.LazyPagingNoticeItemColumn
 import com.ku_stacks.ku_ring.designsystem.kuringtheme.KuringTheme
@@ -216,18 +237,41 @@ private fun DepartmentNoticeScreenContent(
                 selectedDepartmentName = selectedDepartment?.koreanName ?: "",
                 onClick = { scope.launch { sheetState.show() } },
             )
-            LazyPagingNoticeItemColumn(
-                notices = notices,
-                onNoticeClick = onNoticeClick,
-                modifier = Modifier.pullRefresh(refreshState),
-                noticeFilter = { !it.isImportant },
-            )
+
+            if (UnsupportedDepartmentUtil.isUnsupportedDepartment(selectedDepartment?.name)) {
+                UnsupportedDepartmentIndicator(modifier = Modifier.fillMaxSize())
+            } else {
+                LazyPagingNoticeItemColumn(
+                    notices = notices,
+                    onNoticeClick = onNoticeClick,
+                    modifier = Modifier.pullRefresh(refreshState),
+                    noticeFilter = { !it.isImportant },
+                )
+            }
         }
 
         PullRefreshIndicator(
             refreshing = isRefreshing,
             state = refreshState,
             modifier = Modifier.align(Alignment.TopCenter),
+        )
+    }
+}
+
+@Composable
+private fun UnsupportedDepartmentIndicator(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.background(KuringTheme.colors.background)) {
+        Text(
+            text = stringResource(id = R.string.unsupported_department),
+            style = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontFamily = Pretendard,
+                fontWeight = FontWeight(500),
+                color = KuringTheme.colors.textCaption2,
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier.align(Alignment.Center),
         )
     }
 }
