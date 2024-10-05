ca278e211684f4cbfcd8451232d66b642d1a8bc1
Wooyoung Myung <mwy3055@gmail.com>
2024-10-05 14:08:37 +0900

    gradle: 제거되지 않은 build config 제거 (#351)

--- core/util/build.gradle.kts
+++ core/util/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.ku_stacks.ku_ring.buildlogic.dsl.setNameSpace
-import java.util.Properties
 
 plugins {
     kuring("feature")
@@ -8,18 +7,8 @@ plugins {
     kuringPrimitive("retrofit")
 }
 
-val properties = Properties().apply {
-    load(rootProject.file("local.properties").inputStream())
-}
-
 android {
     setNameSpace("util")
-
-    defaultConfig {
-        buildConfigField("String", "APPS_FLYER_DEV_KEY", properties["APPS_FLYER_DEV_KEY"] as? String ?: "")
-        buildConfigField("String", "SENDBIRD_APP_ID", properties["SENDBIRD_APP_ID"] as? String ?: "")
-        buildConfigField("String", "SENDBIRD_API_TOKEN", properties["SENDBIRD_API_TOKEN"] as? String ?: "")
-    }
 }
 
 dependencies {
