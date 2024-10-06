b5baec3b28b042e032e8b9f6ca81fee2c0f709d7
Wooyoung Myung <mwy3055@gmail.com>
2024-10-06 19:59:40 +0900

    feature: 공지 response를 공지 entity로 변환하는 util 추가

--- data/notice/src/main/java/com/ku_stacks/ku_ring/notice/mapper/ResponseToEntity.kt
+++ data/notice/src/main/java/com/ku_stacks/ku_ring/notice/mapper/ResponseToEntity.kt
@@ -2,6 +2,23 @@ package com.ku_stacks.ku_ring.notice.mapper
 
 import com.ku_stacks.ku_ring.local.entity.NoticeEntity
 import com.ku_stacks.ku_ring.remote.notice.response.DepartmentNoticeResponse
+import com.ku_stacks.ku_ring.remote.notice.response.NoticeResponse
+
+fun List<NoticeResponse>.toEntities(startDate: String) = map { it.toNoticeEntity(startDate) }
+
+fun NoticeResponse.toNoticeEntity(startDate: String) = NoticeEntity(
+    articleId = articleId,
+    category = category,
+    department = "",
+    subject = subject,
+    postedDate = postedDate,
+    url = url,
+    isNew = (postedDate >= startDate),
+    isRead = false,
+    isSaved = false,
+    isReadOnStorage = false,
+    isImportant = isImportant,
+)
 
 
 fun List<DepartmentNoticeResponse>.toEntityList(shortName: String, startDate: String) =
