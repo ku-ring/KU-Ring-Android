6177d5019bdcd01166899b3c9472db8dd9d2ca8e
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2024-10-20 16:34:50 +0900

    빌드시 테스트 태스크 제외시키고 CI에서만 돌리기 (#362)

--- build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/FeaturePlugin.kt
+++ build-logic/src/main/kotlin/com/ku_stacks/ku_ring/buildlogic/convention/FeaturePlugin.kt
@@ -1,13 +1,14 @@
 package com.ku_stacks.ku_ring.buildlogic.convention
 
-import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
+import com.android.build.gradle.tasks.factory.AndroidUnitTest
 import com.ku_stacks.ku_ring.buildlogic.dsl.configureAndroidLibrary
 import com.ku_stacks.ku_ring.buildlogic.primitive.CommonAndroidPlugin
 import com.ku_stacks.ku_ring.buildlogic.primitive.HiltPlugin
 import com.ku_stacks.ku_ring.buildlogic.primitive.KotlinPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 
@@ -31,6 +32,12 @@ class FeaturePlugin: Plugin<Project> {
                     isMinifyEnabled = false
                     proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
                 }
+            }
+            tasks.withType(Test::class.java).configureEach {
+                enabled = project.hasProperty("isCI") && project.property("isCI") == "true"
+            }
+            tasks.withType(AndroidUnitTest::class.java).configureEach {
+                enabled = project.hasProperty("isCI") && project.property("isCI") == "true"
             }
         }
     }
