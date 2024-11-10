435b015d262405b4e5824f2a438f7017cc74ec1d
boiledegg <tjrwns2163@naver.com>
2024-11-10 15:07:09 +0900

    [수정] 도메인 엔티티 이름 수정

--- data/domain/src/main/java/com/ku_stacks/ku_ring/domain/LibraryArea.kt
+++
@@ -1,8 +0,0 @@
-package com.ku_stacks.ku_ring.domain
-
-data class LibraryArea(
-    val name: String,
-    val totalSeats: String,
-    val occupiedSeats: String,
-    val availableSeats: String
-)
