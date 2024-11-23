b6eecb80ac7e12ef02448e62192465d48182f7f6
boiledegg <tjrwns2163@naver.com>
2024-11-23 12:34:23 +0900

    [수정] Repository 예외처리 수정

--- data/library/src/main/java/com/ku_stacks/ku_ring/library/repository/LibraryRepository.kt
+++ data/library/src/main/java/com/ku_stacks/ku_ring/library/repository/LibraryRepository.kt
@@ -3,5 +3,5 @@ package com.ku_stacks.ku_ring.library.repository
 import com.ku_stacks.ku_ring.domain.LibraryRoom
 
 interface LibraryRepository {
-    suspend fun getRemainingSeats(): List<LibraryRoom>
+    suspend fun getRemainingSeats(): Result<List<LibraryRoom>>
 }
--- data/library/src/main/java/com/ku_stacks/ku_ring/library/repository/LibraryRepositoryImpl.kt
+++ data/library/src/main/java/com/ku_stacks/ku_ring/library/repository/LibraryRepositoryImpl.kt
@@ -3,17 +3,12 @@ package com.ku_stacks.ku_ring.library.repository
 import com.ku_stacks.ku_ring.domain.LibraryRoom
 import com.ku_stacks.ku_ring.library.mapper.toLibraryAreaList
 import com.ku_stacks.ku_ring.remote.library.LibraryClient
-import retrofit2.HttpException
 import javax.inject.Inject
 
 class LibraryRepositoryImpl @Inject constructor(
     private val libraryClient: LibraryClient
-): LibraryRepository {
-    override suspend fun getRemainingSeats(): List<LibraryRoom> {
-        return try {
-            libraryClient.fetchRoomSeatStatus().toLibraryAreaList()
-        } catch (e: HttpException) {
-            emptyList()
-        }
+) : LibraryRepository {
+    override suspend fun getRemainingSeats(): Result<List<LibraryRoom>> = runCatching {
+        libraryClient.fetchRoomSeatStatus().toLibraryAreaList()
     }
 }
