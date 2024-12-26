b57606505afc2e2acadc4b25b05537ecbdeb9fa8
Wooyoung Myung <mwy3055@gmail.com>
2024-12-26 20:55:46 +0900

    refactor: internal 키워드 추가

--- core/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/components/dragdrop/DragDropUtil.kt
+++ core/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/components/dragdrop/DragDropUtil.kt
@@ -1,6 +1,6 @@
 package com.ku_stacks.ku_ring.designsystem.components.dragdrop
 
-fun <T> MutableList<T>.move(from: Int, to: Int) {
+internal fun <T> MutableList<T>.move(from: Int, to: Int) {
     if (from == to) return
     val element = this.removeAt(from)
     this.add(to, element)
