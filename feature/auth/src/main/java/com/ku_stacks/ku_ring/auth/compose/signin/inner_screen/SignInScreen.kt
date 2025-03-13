e8e521ff61036f48941c439b554c8d30b3925f2f
boiledegg <tjrwns2163@naver.com>
2025-03-13 16:14:52 +0900

    [feat] 키보드액션 추가

--- feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/signin/inner_screen/SignInScreen.kt
+++ feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/signin/inner_screen/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.ku_stacks.ku_ring.auth.compose.signin.inner_screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -20,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -111,7 +115,10 @@ private fun SignInScreen(
             placeholderText = stringResource(sign_in_text_field_placeholder_email),
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next
+                imeAction = ImeAction.Next,
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
             ),
             modifier = Modifier
                 .fillMaxWidth()
@@ -123,7 +130,12 @@ private fun SignInScreen(
             query = password,
             onQueryUpdate = onPasswordTextFieldValueChange,
             placeholderText = stringResource(sign_in_text_field_placeholder_password),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { focusManager.clearFocus() }
+            ),
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier
                 .fillMaxWidth()
