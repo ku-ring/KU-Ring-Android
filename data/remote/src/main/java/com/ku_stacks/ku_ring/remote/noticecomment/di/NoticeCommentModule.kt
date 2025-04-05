2ba9032c8aa2f0fcbd7e7c806632ee7a34bc3e7b
Wooyoung Myung <mwy3055@gmail.com>
2025-04-05 21:01:09 +0900

    feature: 공지 API inject 모듈 추가

---
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/noticecomment/di/NoticeCommentModule.kt
@@ -0,0 +1,27 @@
+package com.ku_stacks.ku_ring.remote.noticecomment.di
+
+import com.ku_stacks.ku_ring.remote.noticecomment.NoticeCommentClient
+import com.ku_stacks.ku_ring.remote.noticecomment.NoticeCommentService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NoticeCommentModule {
+    @Provides
+    @Singleton
+    fun provideNoticeCommentService(@Named("Default") retrofit: Retrofit): NoticeCommentService {
+        return retrofit.create(NoticeCommentService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoticeCommentClient(noticeCommentService: NoticeCommentService): NoticeCommentClient {
+        return NoticeCommentClient(noticeCommentService)
+    }
+}
