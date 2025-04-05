33eb8e6ce79fce112e07ea7858a593a41df55e13
Wooyoung Myung <mwy3055@gmail.com>
2025-04-05 21:00:58 +0900

    feature: 공지 API retrofit 클라이언트 구현

---
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/noticecomment/NoticeCommentService.kt
@@ -0,0 +1,40 @@
+package com.ku_stacks.ku_ring.remote.noticecomment
+
+import com.ku_stacks.ku_ring.remote.noticecomment.request.NoticeCommentCreateRequest
+import com.ku_stacks.ku_ring.remote.noticecomment.request.NoticeCommentEditRequest
+import com.ku_stacks.ku_ring.remote.noticecomment.response.NoticeCommentResponse
+import com.ku_stacks.ku_ring.remote.util.DefaultResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface NoticeCommentService {
+    @POST("v2/notices/{id}/comments")
+    suspend fun createComment(
+        @Path("id") noticeId: Int,
+        @Body request: NoticeCommentCreateRequest,
+    ): DefaultResponse
+
+    @POST("v2/notices/{id}/comments/{commentId}")
+    suspend fun editComment(
+        @Path("id") noticeId: Int,
+        @Path("commentId") commentId: Int,
+        @Body request: NoticeCommentEditRequest,
+    ): DefaultResponse
+
+    @DELETE("v2/notices/{id}/comments/{commentId}")
+    suspend fun deleteComment(
+        @Path("id") noticeId: Int,
+        @Path("commentId") commentId: Int,
+    ): String
+
+    @GET("v2/notices/{id}/comments")
+    suspend fun getComment(
+        @Path("id") noticeId: Int,
+        @Query("cursor") cursor: String?,
+        @Query("size") size: Int,
+    ): NoticeCommentResponse
+}
