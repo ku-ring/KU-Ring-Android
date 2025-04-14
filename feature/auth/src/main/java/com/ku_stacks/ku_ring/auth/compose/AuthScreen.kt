f901c62b1a12809498811009d7d0f9f9ddc07fdc
boiledegg <tjrwns2163@naver.com>
2025-04-15 01:03:37 +0900

    [feat] 뷰모델 공유 로직

--- feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/AuthScreen.kt
+++ feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/AuthScreen.kt
@@ -4,8 +4,11 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntOffset
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -59,3 +62,12 @@ internal fun animationDirection(initialState: NavBackStackEntry, targetState: Na
 
 internal val NavBackStackEntry.screenOrder: Int
     get() = AuthDestination.getOrder(destination)
+
+@Composable
+internal inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavHostController): T {
+    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return hiltViewModel(parentEntry)
+}
