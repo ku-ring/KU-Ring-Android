6afacea282dae9c4007db3579ba44cf52ed17774
boiledegg <tjrwns2163@naver.com>
2025-04-24 02:48:09 +0900

    [feat] 비밀번호 재설정 플로우 공유 뷰모델 추가

--- feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/reset_password/ResetPasswordNavigation.kt
+++ feature/auth/src/main/java/com/ku_stacks/ku_ring/auth/compose/reset_password/ResetPasswordNavigation.kt
@@ -7,27 +7,32 @@ import androidx.navigation.compose.navigation
 import com.ku_stacks.ku_ring.auth.compose.AuthDestination
 import com.ku_stacks.ku_ring.auth.compose.reset_password.inner_screen.EmailVerificationScreen
 import com.ku_stacks.ku_ring.auth.compose.reset_password.inner_screen.ResetPasswordScreen
+import com.ku_stacks.ku_ring.auth.compose.sharedViewModel
 
 internal fun NavGraphBuilder.resetPasswordNavGraph(
     navController: NavHostController,
 ) {
     navigation<AuthDestination.ResetPassword>(
-        startDestination = AuthDestination.ResetPasswordEmailVerification,
+        startDestination = AuthDestination.ResetPasswordSetPassword,
     ) {
-        composable<AuthDestination.ResetPasswordEmailVerification> {
+        composable<AuthDestination.ResetPasswordEmailVerification> { backStackEntry ->
+            val viewModel = backStackEntry.sharedViewModel<ResetPasswordViewModel>(navController)
             EmailVerificationScreen(
                 onNavigateUp = navController::navigateUp,
                 onNavigateToPassword = {
                     navController.navigate(AuthDestination.ResetPasswordSetPassword)
                 },
+                viewModel = viewModel
             )
         }
-        composable<AuthDestination.ResetPasswordSetPassword> {
+        composable<AuthDestination.ResetPasswordSetPassword> { backStackEntry ->
+            val viewModel = backStackEntry.sharedViewModel<ResetPasswordViewModel>(navController)
             ResetPasswordScreen(
                 onNavigateUp = navController::navigateUp,
                 onNavigateToSignIn = {
                     navController.popBackStack(AuthDestination.SignIn, inclusive = false)
                 },
+                viewModel = viewModel
             )
         }
     }
