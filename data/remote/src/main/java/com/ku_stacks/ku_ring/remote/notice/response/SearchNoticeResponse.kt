01ddaa8501046e6631a4db696d19bc664d720ef4
Wooyoung Myung <mwy3055@gmail.com>
2025-04-27 20:03:54 +0900

    feature: 공지 API 응답에 id 추가

--- data/remote/src/main/java/com/ku_stacks/ku_ring/remote/notice/response/SearchNoticeResponse.kt
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/notice/response/SearchNoticeResponse.kt
@@ -3,6 +3,7 @@ package com.ku_stacks.ku_ring.remote.notice.response
 import com.google.gson.annotations.SerializedName
 
 data class SearchNoticeResponse(
+    @SerializedName("id") val id: Int,
     @SerializedName("articleId") val articleId: String,
     @SerializedName("postedDate") val postedDate: String,
     @SerializedName("subject") val subject: String,
