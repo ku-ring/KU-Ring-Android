a6de098b8e0ca5f46e7e4cb266458513dda82cdf
boiledegg <tjrwns2163@naver.com>
2025-08-05 17:25:08 +0900

    feature: 폰트 토큰 구현

--- core/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/kuringtheme/values/Typography.kt
+++ core/designsystem/src/main/java/com/ku_stacks/ku_ring/designsystem/kuringtheme/values/Typography.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.ku_stacks.ku_ring.designsystem.R
 
@@ -25,59 +26,86 @@ val Pretendard = FontFamily(
     Font(R.font.pretendard_thin, FontWeight.Thin),
 )
 
-// fontSize, fontWeight, lineHeight, letterSpacing(optional)
+// fontFamily, fontSize, fontWeight, lineHeight, letterSpacing(optional)
+internal object KuringTypographyTokens {
+    val NoticeTitle =
+        KuringDefaultTextStyle.copy(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 1.5.em,
+        )
+    val Title1 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 1.42.em,
+        )
+    val Title2 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 1.5.em,
+            letterSpacing = (-0.41).em,
+        )
+    val ViewTitle =
+        KuringDefaultTextStyle.copy(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            lineHeight = 1.48.em,
+            letterSpacing = (-0.41).sp,
+        )
+    val TitleSB =
+        KuringDefaultTextStyle.copy(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold,
+            lineHeight = 1.5.em,
+        )
+    val Body1 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Medium,
+            lineHeight = 1.63.em,
+        )
+    // Body2-Regular, Body2-SemiBold는  KuringTheme.typography.Body2.copy(fontWeight = FontWeight.Regular)처럼 사용
+    val Body2 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            lineHeight = 1.5.em,
+        )
+    val Caption1 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+            lineHeight = 1.63.em,
+        )
+    val Caption1_1 =
+        KuringDefaultTextStyle.copy(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            lineHeight = 1.63.em,
+        )
+    val Tag =
+        KuringDefaultTextStyle.copy(
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            lineHeight = 1.5.em,
+        )
+    val InputField =
+        KuringDefaultTextStyle.copy(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            lineHeight = 1.63.em,
+        )
+    val BottomNavigationDefault =
+        KuringDefaultTextStyle.copy(
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Medium,
+            lineHeight = 1.63.em,
+        )
+}
 
-internal val NoticeTitle = TextStyle(
-    fontSize = 20.sp,
-    fontWeight = FontWeight.Bold,
-    lineHeight = (20 * 1.5f).sp,
-)
-internal val Title1 = TextStyle(
-    fontSize = 24.sp,
-    fontWeight = FontWeight.Bold,
-    lineHeight = (24 * 1.42f).sp,
-)
-internal val Title2 = TextStyle(
-    fontSize = 18.sp,
-    fontWeight = FontWeight.Bold,
-    lineHeight = 27.sp,
-    letterSpacing = (-0.41).sp,
-)
-internal val ViewTitle = TextStyle(
-    fontSize = 18.sp,
-    fontWeight = FontWeight.SemiBold,
-    lineHeight = (18 * 1.48f).sp,
-    letterSpacing = (-0.41).sp,
-)
-internal val Body1 = TextStyle(
-    fontSize = 15.sp,
-    fontWeight = FontWeight.Medium,
-    lineHeight = (15 * 1.63f).sp,
-)
-
-// Body2-Regular 등은 KuringTheme.typography.Body2.copy(fontWeight = FontWeight.Regular)처럼 사용
-internal val Body2 = TextStyle(
-    fontSize = 16.sp,
-    fontWeight = FontWeight.Medium,
-    lineHeight = (16 * 1.5f).sp,
-)
-internal val Caption1 = TextStyle(
-    fontSize = 14.sp,
-    fontWeight = FontWeight.Normal,
-    lineHeight = (14 * 1.63f).sp,
-)
-internal val Tag = TextStyle(
-    fontSize = 12.sp,
-    fontWeight = FontWeight.SemiBold,
-    lineHeight = (12 * 1.63f).sp,
-)
-internal val TextInputField = TextStyle(
-    fontSize = 16.sp,
-    fontWeight = FontWeight.Normal,
-    lineHeight = (16 * 1.63f).sp,
-)
-internal val BottomNavigationDefault = TextStyle(
-    fontSize = 10.sp,
-    fontWeight = FontWeight.Medium,
-    lineHeight = (10 * 1.63f).sp,
-)
+internal val KuringDefaultTextStyle =
+    TextStyle.Default.copy(
+        fontFamily = Pretendard,
+    )
