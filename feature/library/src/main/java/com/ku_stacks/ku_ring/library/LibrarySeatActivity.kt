47bffca4df3abff66cad7812adbed1172af974ef
Wooyoung Myung <mwy3055@gmail.com>
2025-08-24 15:40:57 +0900

    refactor: enableEdgeToEdge() 제거

--- feature/library/src/main/java/com/ku_stacks/ku_ring/library/LibrarySeatActivity.kt
+++ feature/library/src/main/java/com/ku_stacks/ku_ring/library/LibrarySeatActivity.kt
@@ -9,7 +9,6 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
@@ -23,7 +22,6 @@ class LibrarySeatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             KuringTheme {
                 LibrarySeatScreen(
