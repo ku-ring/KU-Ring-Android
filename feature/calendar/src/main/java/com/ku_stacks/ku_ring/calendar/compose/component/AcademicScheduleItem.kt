57b0b1e4f70c3b43333156e5d4d6a6c71a68704f
boiledegg <tjrwns2163@naver.com>
2025-09-15 11:34:34 +0900

    fix: 학사 일점 컴포넌트 패딩 조정

--- feature/calendar/src/main/java/com/ku_stacks/ku_ring/calendar/compose/component/AcademicScheduleItem.kt
+++ feature/calendar/src/main/java/com/ku_stacks/ku_ring/calendar/compose/component/AcademicScheduleItem.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -40,7 +42,9 @@ internal fun AcademicScheduleItem(
         Row(
             modifier = modifier
                 .fillMaxWidth()
+                .heightIn(46.dp)
                 .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             ScheduleColorIndicator(scheduleType = scheduleType)
             Spacer(modifier = Modifier.width(8.dp))
@@ -51,7 +55,7 @@ internal fun AcademicScheduleItem(
                     color = KuringTheme.colors.textTitle,
                     maxLines = 1,
                 )
-                Spacer(modifier = Modifier.height(4.dp))
+                Spacer(modifier = Modifier.height(8.dp))
                 Text(
                     text = "$startTime - $endTime",
                     style = KuringTheme.typography.tag2,
