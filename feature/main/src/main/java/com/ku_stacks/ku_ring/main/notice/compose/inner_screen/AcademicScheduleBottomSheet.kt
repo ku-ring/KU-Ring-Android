d7448732fa351e4acb527d3e0ea98e21c3ff401a
boiledegg <tjrwns2163@naver.com>
2025-09-25 20:25:24 +0900

    mod: 바텀시트 문자열 높이 조정

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/compose/inner_screen/AcademicScheduleBottomSheet.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/notice/compose/inner_screen/AcademicScheduleBottomSheet.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -132,12 +133,14 @@ private fun AcademicEventBottomSheetContent(
                     style = KuringTheme.typography.body1,
                     color = KuringTheme.colors.textTitle,
                     maxLines = 1,
+                    modifier = Modifier.heightIn(min = 24.dp),
                 )
                 Text(
                     text = academicEvent.period,
                     style = KuringTheme.typography.tag2,
                     color = KuringTheme.colors.textCaption1,
                     maxLines = 1,
+                    modifier = Modifier.heightIn(min = 18.dp),
                 )
             }
         }
