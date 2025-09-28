492919c07292a0020894cd0f7549f07216137f68
boiledegg <tjrwns2163@naver.com>
2025-09-28 23:21:39 +0900

    feat: 학사일정 저장소 관련 테스트 추가

---
+++ data/academicevent/src/test/java/com/ku_stacks/ku_ring/academicevent/AcademicEventTestUtil.kt
@@ -0,0 +1,32 @@
+package com.ku_stacks.ku_ring.academicevent
+
+import com.ku_stacks.ku_ring.remote.academicevent.response.AcademicEventListResponse
+import com.ku_stacks.ku_ring.remote.academicevent.response.AcademicEventResponse
+
+object AcademicEventTestUtil {
+    fun mockAcademicEventListResponse() = AcademicEventListResponse(
+        code = 200,
+        message = "조회되었습니다.",
+        data = listOf(
+            AcademicEventResponse(
+                id = 2417,
+                eventUid = "EC66FB8098",
+                summary = "등록금의 6분의 5해당액 반환",
+                description = null,
+                category = "ETC",
+                startTime = "2025-03-04T00:00:00",
+                endTime = "2025-04-03T00:00:00"
+            ),
+            AcademicEventResponse(
+
+                id = 2429,
+                eventUid = "41C75146F2",
+                summary = "제적생변동상황 통계작업(휴복학처리불가)",
+                description = null,
+                category = "ETC",
+                startTime = "2025-03-31T00:00:00",
+                endTime = "2025-04-12T00:00:00"
+            ),
+        )
+    )
+}
---
+++ data/academicevent/src/test/java/com/ku_stacks/ku_ring/academicevent/AcademicRepositoryTest.kt
@@ -0,0 +1,79 @@
+package com.ku_stacks.ku_ring.academicevent
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.ku_stacks.ku_ring.academicevent.mapper.toDomain
+import com.ku_stacks.ku_ring.academicevent.mapper.toEntity
+import com.ku_stacks.ku_ring.academicevent.repository.AcademicEventRepository
+import com.ku_stacks.ku_ring.academicevent.repository.AcademicEventRepositoryImpl
+import com.ku_stacks.ku_ring.local.room.AcademicEventDao
+import com.ku_stacks.ku_ring.local.room.KuRingDatabase
+import com.ku_stacks.ku_ring.remote.academicevent.AcademicEventClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class AcademicRepositoryTest {
+    private val academicEventClient = Mockito.mock(AcademicEventClient::class.java)
+    private lateinit var repository: AcademicEventRepository
+    private lateinit var database: KuRingDatabase
+    private lateinit var academicEventDao: AcademicEventDao
+    private val ioDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(ioDispatcher)
+        initDatabase()
+        academicEventDao = database.academicEventDao()
+        repository =
+            AcademicEventRepositoryImpl(academicEventDao, academicEventClient, ioDispatcher)
+    }
+
+    private fun initDatabase() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        database = Room
+            .inMemoryDatabaseBuilder(context, KuRingDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        database.close()
+    }
+
+    @Test
+    fun `insert and get academic event test`() = runTest {
+        //given
+        val response = AcademicEventTestUtil.mockAcademicEventListResponse()
+        val entities = response.data.map { it.toEntity() }
+        academicEventDao.insertAcademicEvents(entities)
+
+        //when
+        val result = repository.getAcademicEvents("2025-03-01", "2025-04-30")
+
+        // then
+        val expected = entities.first().toDomain()
+        with(result.first()) {
+            assertEquals(expected.id, id)
+            assertEquals(expected.summary, summary)
+            assertEquals(expected.category, category)
+            assertEquals(expected.startDateTime, startDateTime)
+            assertEquals(expected.endDateTime, endDateTime)
+        }
+    }
+}
