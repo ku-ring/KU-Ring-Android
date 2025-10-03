fd6150780a573a90cbdeeb372443e47628fea3ca
boiledegg <tjrwns2163@naver.com>
2025-10-03 18:44:04 +0900

    docs: 함수 kdoc 설명 추가

--- domain/academicevent/src/main/java/com/ku_stacks/ku_ring/domain/academicevent/repository/AcademicEventRepository.kt
+++ domain/academicevent/src/main/java/com/ku_stacks/ku_ring/domain/academicevent/repository/AcademicEventRepository.kt
@@ -3,11 +3,21 @@ package com.ku_stacks.ku_ring.domain.academicevent.repository
 import com.ku_stacks.ku_ring.domain.AcademicEvent
 
 interface AcademicEventRepository {
+    /**
+     * fetches academic events that have a date range which intersects with the given range from the remote and saves in the local database
+     * @param startDate start date of the academic events (format: yyyy-MM-dd, no limit if null)
+     * @param endDate end date of the academic events (format: yyyy-MM-dd, no limit if null)
+     */
     suspend fun fetchAcademicEventsFromRemote(
         startDate: String? = null,
         endDate: String? = null,
     ): Result<Unit>
 
+    /**
+     * fetches academic events from the local database that have a date range which intersects with the given range
+     * @param startDate start date of the academic events (format: yyyy-MM-dd, no limit if null)
+     * @param endDate end date of the academic events (format: yyyy-MM-dd, no limit if null)
+     */
     suspend fun getAcademicEvents(
         startDate: String,
         endDate: String,
