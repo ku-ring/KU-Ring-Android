1fd4867b0d0f7a860314fa9d3aad4107dc4dcd3b
boiledegg <tjrwns2163@naver.com>
2025-10-06 17:33:00 +0900

    refactor: 유즈케이스 내 컬렉션 타입 변환 로직 수정

--- domain/academicevent/src/main/java/com/ku_stacks/ku_ring/domain/academicevent/usecase/GetAcademicEventsUseCase.kt
+++ domain/academicevent/src/main/java/com/ku_stacks/ku_ring/domain/academicevent/usecase/GetAcademicEventsUseCase.kt
@@ -2,7 +2,9 @@ package com.ku_stacks.ku_ring.domain.academicevent.usecase
 
 import com.ku_stacks.ku_ring.domain.AcademicEvent
 import com.ku_stacks.ku_ring.domain.academicevent.repository.AcademicEventRepository
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.plus
 import javax.inject.Inject
 
 class GetAcademicEventsUseCase @Inject constructor(
@@ -15,7 +17,7 @@ class GetAcademicEventsUseCase @Inject constructor(
         .getAcademicEvents(startDate.toString(), endDate.toString())
         .mapByDate(startDate, endDate)
 
-    private fun Result<List<AcademicEvent>>.mapByDate(
+    /*private fun Result<List<AcademicEvent>>.mapByDate(
         startDate: LocalDate,
         endDate: LocalDate,
     ) = map { academicEvents ->
@@ -31,5 +33,19 @@ class GetAcademicEventsUseCase @Inject constructor(
                 }
             }
         }
+    }*/
+
+    private fun Result<List<AcademicEvent>>.mapByDate(
+        startDate: LocalDate,
+        endDate: LocalDate,
+    ) = map { academicEvents ->
+        academicEvents.flatMap { event ->
+            val from = maxOf(event.startDateTime.date, startDate)
+            val to = minOf(event.endDateTime.date, endDate)
+            if (from > to) emptyList()
+            else generateSequence(from) {
+                it.plus(1, DateTimeUnit.DAY).takeIf { date -> date <= to }
+            }.map { it.toString() to event }.toList()
+        }.groupBy({ it.first }, { it.second })
     }
 }
