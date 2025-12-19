84fed8980ccfe2a352a2901fb48e6af036aa61c4
boiledegg <tjrwns2163@naver.com>
2025-12-19 23:30:49 +0900

    fix: 리소스 누수 요소 수정

--- data/place/src/main/java/com/ku_stacks/ku_ring/place/datasource/PlaceDataSource.kt
+++ data/place/src/main/java/com/ku_stacks/ku_ring/place/datasource/PlaceDataSource.kt
@@ -14,13 +14,12 @@ class PlaceDataSource @Inject constructor(
     @param:ApplicationContext private val context: Context,
     @param:IODispatcher private val ioDispatcher: CoroutineDispatcher,
 ) {
-    internal suspend fun getJsonPlaces() : List<JsonPlace> = withContext(ioDispatcher){
-        val assetManager = context.assets
-        val inputStream = assetManager.open("KonkukPlaces.json")
-        val jsonString = inputStream.bufferedReader().use { it.readText() }
-        val jsonParser = Json { ignoreUnknownKeys = true }
-
+    internal suspend fun getJsonPlaces(): List<JsonPlace> = withContext(ioDispatcher) {
         try {
+            val jsonString = context.assets.open("KonkukPlaces.json").use { inputStream ->
+                inputStream.bufferedReader().use { it.readText() }
+            }
+            val jsonParser = Json { ignoreUnknownKeys = true }
             jsonParser.decodeFromString<List<JsonPlace>>(jsonString)
         } catch (e: Exception) {
             Timber.e(e)
