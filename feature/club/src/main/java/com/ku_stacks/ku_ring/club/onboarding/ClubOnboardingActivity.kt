984d65a678555fe1d7d498ba5265aa50d4faf5e8
Wooyoung Myung <mwy3055@gmail.com>
2026-02-07 20:30:41 +0900

    feature: 온보딩 activity와 UI 연결

--- feature/club/src/main/java/com/ku_stacks/ku_ring/club/onboarding/ClubOnboardingActivity.kt
+++ feature/club/src/main/java/com/ku_stacks/ku_ring/club/onboarding/ClubOnboardingActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.ku_stacks.ku_ring.designsystem.kuringtheme.KuringTheme
 
 class ClubOnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            KuringTheme {
+                ClubOnboardingScreen(onClose = ::finish)
+            }
         }
     }
 }
