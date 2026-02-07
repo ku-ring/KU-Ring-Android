39166f98cb9d58c7c1d7500d5e3079a205db7c40
Wooyoung Myung <mwy3055@gmail.com>
2026-02-07 17:12:29 +0900

    feature: 온보딩 activity 추가

---
+++ feature/club/src/main/java/com/ku_stacks/ku_ring/club/onboarding/ClubOnboardingActivity.kt
@@ -0,0 +1,16 @@
+package com.ku_stacks.ku_ring.club.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class ClubOnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
