58278321054dd6c591cd84253e05bad0e1a06ed2
boiledegg <tjrwns2163@naver.com>
2026-02-08 21:25:04 +0900

    feature: 동아리 탭에서 사용할 컴포넌트 구현
    - 동아리 목록 바텀시트
    - 동아리 목록 칩버튼 그룹
    - 동아리 카테고리 탭
    - 동아리 탑바

---
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/club/compose/type/ClubTabItem.kt
@@ -0,0 +1,9 @@
+package com.ku_stacks.ku_ring.main.club.compose.type
+
+enum class ClubTabItem(val koreanName: String) {
+    TOTAL("전체"),
+    ACADEMIC("학술활동"),
+    CULTURE_ARTS("문화예술"),
+    SOCIAL("사회가치"),
+    ACTIVITIES("야외활동"),
+}
