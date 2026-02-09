373dc1f93c55060205eb3c56f4699e7ff1a4f01d
Wooyoung Myung <mwy3055@gmail.com>
2026-02-09 22:28:26 +0900

    fix: repository impl에서 중복된 코드 제거

--- data/club/src/main/java/com/ku_stacks/ku_ring/club/ClubRepositoryImpl.kt
+++ data/club/src/main/java/com/ku_stacks/ku_ring/club/ClubRepositoryImpl.kt
@@ -12,24 +12,24 @@ class ClubRepositoryImpl @Inject constructor(
     override suspend fun subscribeClub(clubId: Int): Result<Int> {
         val response = clubClient.subscribeClub(clubId)
         return when {
-            response.isSuccess && response.data != null -> Result.success(response.data!!.bookmarkCount)
-            else -> Result.failure(IllegalStateException("구독에 실패했습니다. 잠시 후 다시 시도해 주세요."))
+            response.isSuccessAndDataExists -> Result.success(response.data!!.bookmarkCount)
+            else -> Result.failure(IllegalStateException(response.resultMsg))
         }
     }
 
     override suspend fun unsubscribeClub(clubId: Int): Result<Int> {
         val response = clubClient.unsubscribeClub(clubId)
         return when {
-            response.isSuccess && response.data != null -> Result.success(response.data!!.bookmarkCount)
-            else -> Result.failure(IllegalStateException("구독 해지에 실패했습니다. 잠시 후 다시 시도해 주세요."))
+            response.isSuccessAndDataExists -> Result.success(response.data!!.bookmarkCount)
+            else -> Result.failure(IllegalStateException(response.resultMsg))
         }
     }
 
     override suspend fun getClubDetail(clubId: Int): Result<Club> {
         val response = clubClient.getClubDetail(clubId)
         return when {
-            response.isSuccess && response.data != null -> Result.success(response.data!!.toClub())
-            else -> Result.failure(IllegalStateException("동아리 정보를 불러오는데 실패했습니다. 잠시 후 다시 시도해 주세요."))
+            response.isSuccessAndDataExists -> Result.success(response.data!!.toClub())
+            else -> Result.failure(IllegalStateException(response.resultMsg))
         }
     }
 }
