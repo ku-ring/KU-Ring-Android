6182619efeff6f77ca7dc23c41bb4753cb5a8edf
Wooyoung Myung <mwy3055@gmail.com>
2026-02-16 21:49:05 +0900

    temp: API 개발 전 임시 테스트 코드 추가 (API 완료되면 삭제해야)

--- feature/club/src/main/java/com/ku_stacks/ku_ring/club/detail/ClubDetailViewModel.kt
+++ feature/club/src/main/java/com/ku_stacks/ku_ring/club/detail/ClubDetailViewModel.kt
@@ -7,14 +7,24 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ku_stacks.ku_ring.club.R
+import com.ku_stacks.ku_ring.domain.Club
+import com.ku_stacks.ku_ring.domain.ClubAffiliation
+import com.ku_stacks.ku_ring.domain.ClubCategory
+import com.ku_stacks.ku_ring.domain.ClubDivision
+import com.ku_stacks.ku_ring.domain.ClubLocation
+import com.ku_stacks.ku_ring.domain.ClubRecruitment
+import com.ku_stacks.ku_ring.domain.RecruitmentStatus
 import com.ku_stacks.ku_ring.domain.club.ClubRepository
+import com.ku_stacks.ku_ring.util.now
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.datetime.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -40,6 +50,36 @@ class ClubDetailViewModel @Inject constructor(
 
     fun loadClub() {
         viewModelScope.launch {
+
+            // TODO: delete this mock
+            delay(300)
+            _clubUiState.value = ClubDetailUiState.Success(
+                Club(
+                    id = 1,
+                    name = "쿠링 학술 소모임",
+                    summary = "안드로이드를 공부하는 모임입니다.",
+                    category = ClubCategory.ACADEMIC,
+                    affiliation = ClubAffiliation.CENTRAL,
+                    division = ClubDivision.ENGINEERING,
+                    description = "상세 설명".repeat(50),
+                    location = ClubLocation("공학관", "A101", 37.541635, 127.0787904),
+                    applyQualification = "컴퓨터공학과 학생",
+                    recruitment = ClubRecruitment(
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        RecruitmentStatus.RECRUITING,
+                        "https://www.youtube.com/@Google"
+                    ),
+                    webUrl = listOf(
+                        "https://instagram.com/kuring.official",
+                        "https://youtube.com/@Google"
+                    ),
+                    posterImageUrl = null,
+                    descriptionImageUrl = null,
+                    isSubscribed = true,
+                    subscribeCount = 100
+                )
+            )
             clubRepository.getClubDetail(clubId).onSuccess {
                 _clubUiState.value = ClubDetailUiState.Success(it)
             }.onFailure {
