c1a860b95d48a416087be879f0ebc02de77744ac
Seokjun Lee <tjrwns2163@naver.com>
2026-02-22 23:09:49 +0900

    test: 테스트 코드에서 중복된 어노테이션 제거

--- data/local/src/test/java/com/ku_stacks/ku_ring/local/dao/PushDaoTest.kt
+++ data/local/src/test/java/com/ku_stacks/ku_ring/local/dao/PushDaoTest.kt
@@ -10,11 +10,8 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
 @Config(sdk = [24])
 class PushDaoTest : LocalDbAbstract() {
 
