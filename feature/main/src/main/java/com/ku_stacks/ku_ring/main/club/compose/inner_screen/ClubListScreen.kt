f62940a19225c9f278209642e347096d7eab68bc
Seokjun Lee <tjrwns2163@naver.com>
2026-03-02 13:43:22 +0900

    qa: 동아리 목록 화면 컴포넌트의 패딩 문제 해결

--- feature/main/src/main/java/com/ku_stacks/ku_ring/main/club/compose/inner_screen/ClubListScreen.kt
+++ feature/main/src/main/java/com/ku_stacks/ku_ring/main/club/compose/inner_screen/ClubListScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -178,6 +180,8 @@ private fun ClubListScreen(
             modifier = Modifier
                 .padding(innerPadding)
         ) {
+            Spacer(modifier = Modifier.height(8.dp))
+
             ClubTabRow(
                 pagerState = pagerState,
             )
