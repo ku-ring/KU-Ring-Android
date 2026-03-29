5cf2d3f817b165ba0ac2acca8ef256415afe600e
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-03-29 21:42:14 +0900

    feat: Navigator 싱글톤 추가 및 NavigationController 삭제
    Nav3 스타일의 Navigator 클래스를 core/navigation에 추가하고
    더 이상 필요 없는 NavigationController 인터페이스를 삭제

---
+++ core/navigation/src/main/java/com/ku_stacks/ku_ring/navigation/Navigator.kt
@@ -0,0 +1,26 @@
+package com.ku_stacks.ku_ring.navigation
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.navigation3.runtime.NavKey
+import com.ku_stacks.ku_ring.navigation.keys.SplashKey
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Navigator @Inject constructor() {
+    val backStack: SnapshotStateList<NavKey> = mutableStateListOf(SplashKey)
+
+    fun navigate(key: NavKey) {
+        backStack.add(key)
+    }
+
+    fun goBack(): Boolean {
+        return backStack.removeLastOrNull() != null
+    }
+
+    fun replaceAll(key: NavKey) {
+        backStack.clear()
+        backStack.add(key)
+    }
+}
