2bb9a21a893812bb84c0af58f1529fb7fd890e71
Seokjun Lee <tjrwns2163@naver.com>
2026-04-30 23:09:53 +0900

    refactor: Staff API 수정
    - DefaultResponse를 적용
    - Gson에서 Kotlin Serialization으로 마이그레이션

--- data/remote/src/main/java/com/ku_stacks/ku_ring/remote/staff/StaffService.kt
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/staff/StaffService.kt
@@ -1,6 +1,7 @@
 package com.ku_stacks.ku_ring.remote.staff
 
-import com.ku_stacks.ku_ring.remote.staff.response.SearchStaffListResponse
+import com.ku_stacks.ku_ring.remote.staff.response.SearchStaffDataResponse
+import com.ku_stacks.ku_ring.remote.util.DefaultResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,5 +9,5 @@ interface StaffService {
     @GET("v2/staffs/search")
     suspend fun fetchStaffs(
         @Query("content") content: String,
-    ): SearchStaffListResponse
+    ): DefaultResponse<SearchStaffDataResponse>
 }
--- data/remote/src/main/java/com/ku_stacks/ku_ring/remote/staff/di/StaffModule.kt
+++ data/remote/src/main/java/com/ku_stacks/ku_ring/remote/staff/di/StaffModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 object StaffModule {
     @Provides
     @Singleton
-    fun provideSearchService(@Named("Default") retrofit: Retrofit): StaffService {
+    fun provideSearchService(@Named("KotlinxSerialization") retrofit: Retrofit): StaffService {
         return retrofit.create(StaffService::class.java)
     }
 }
